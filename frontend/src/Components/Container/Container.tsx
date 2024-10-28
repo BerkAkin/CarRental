10cf3896eac7a6c7ac4f6b11d40da4a756d9ef5f
Berk Akın <berkakin99@hotmail.com>
2024-10-28 16:44:59 +0300

    fix typo

--- frontend/src/Components/Container/Container.tsx
+++ frontend/src/Components/Container/Container.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Infobar from '../InfoBar/Infobar'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+import React from "react";
+import Infobar from "../InfoBar/Infobar";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 function Container() {
   return (
     <div>
-        Container
-      <Infobar/>
-      <Navbar/>
-      <Footer/>  
+      Container
+      <Infobar />
+      <Navbar />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
