49ba46cbabdbe177d4c57b31fd7cb4f9651d08d5
Berk Akın <berkakin99@hotmail.com>
2024-10-29 01:05:54 +0300

    REFACTOR: removed whitespaces

--- frontend/src/Components/Navbar/TopNav/TopNav.tsx
+++ frontend/src/Components/Navbar/TopNav/TopNav.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import styles from "./style.module.css";
-import ListElement from "./ListElement/ListElement";
+import ListElement from "../../ListElement/ListElement";
 
 function TopNav() {
   return (
     <>
       <div className={`${styles.navbarBg} container-fluid`}>
-        <div className="container pt-2 ms-4">
+        <div className="container pt-2 ms-5">
           <div className="row pt-1 ">
             <ul className="ms-2 d-flex">
-              <ListElement
-                text="info@flexper.com.tr"
-                isHref={true}
-                href="mailto:info@flexper.com.tr"
-              />
+              <ListElement text="info@flexper.com.tr" fs="0.8em" boldness="500" isHref={true} href="mailto:info@flexper.com.tr" color="white" isBold = {true} isHover = {false}/>
               <div className="vr ms-4 me-3"></div>
-              <ListElement text=" +90 543 353 97 37" isHref={false} href="" />
+              <ListElement text=" +90 543 353 97 37" fs="0.8em" boldness="500" isHref={false} href="" color="white" isBold={true} isHover={false}/>
               <div className="vr ms-3 me-3"></div>
-              <ListElement text=" WhatsApp" isHref={false} href="" />
+              <ListElement text=" WhatsApp" fs="0.8em" boldness="500" isHref={false} href="" color="white"  isBold={true} isHover={false} />
             </ul>
           </div>
         </div>
