aaa5b9b33e72db3d7bc6415178490fc490402f14
Berk Akın <berkakin99@hotmail.com>
2024-10-30 20:38:53 +0300

    FEAT: Created Image Component to show images

---
+++ frontend/src/Components/Image/Image.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "./styles.module.css"
+
+interface ImageParams{
+    URL: string;
+    Width : string;
+    Height : string;
+    Alt? : string;
+}
+
+function Image({URL, Width, Height, Alt}: ImageParams) {
+  return (
+    <>
+    <img src={URL} width={Width} height={Height} alt={Alt}></img>
+    </>
+  )
+}
+
+export default Image
