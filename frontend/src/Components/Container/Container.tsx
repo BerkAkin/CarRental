1a556a766f8bbcfb1fd8ade38c0b07694937df5d
Berk Akın <berkakin99@hotmail.com>
2024-10-30 20:38:40 +0300

    FEAT: Added innercontainer component for showing page inner content

--- frontend/src/Components/Container/Container.tsx
+++ frontend/src/Components/Container/Container.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../app.css";
 import TopNav from "../Navbar/TopNav/TopNav";
 import BotNav from "../Navbar/BotNav/BotNav";
+import InnerContainer from "../InnerContainer/InnerContainer";
 
 function Container() {
   return (
     <div>
       <TopNav />
       <BotNav/>
+      <InnerContainer/>
     </div>
   );
 }
