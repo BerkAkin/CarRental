bb2c086eb18ea32e9637e621939ed6ebf4d67874
Berk Akın <berkakin99@hotmail.com>
2024-10-31 20:15:29 +0300

    STYLE FIX: removed unnecessary classes and image imported

--- frontend/src/Components/LandingContainer/LandingContainer.tsx
+++ frontend/src/Components/LandingContainer/LandingContainer.tsx
@@ -2,22 +2,23 @@ import React from 'react'
 import Image from '../Image/Image'
 import Button from '../Button/Button'
 import styles from './styles.module.css'
+import images from '../../assets/images/LandingImages/images';
 
 function LandingContainer() {
     return (
         <div>
             <div className='container-fluid mt-5'>
                 <div className='row'>
-                    <div className='col-4 d-flex justify-content-center flex-column mt-5'>
-                        <div className='row mt-5 ps-3'>
+                    <div className='col-4 d-flex justify-content-center flex-column'>
+                        <div className='row ps-3 '>
                             <h1 id={styles["landingText"]}>Her şey dahil araç abonelik sistemi</h1>
                         </div>
-                        <div className='row mt-5  align-items-end h-25 d-flex justify-content-center '>
+                        <div className='row mt-5 align-items-end h-25 d-flex justify-content-center '>
                             <Button Text='Detay' BgColor='#E00000' Color='#FFFFFF' Border="none" CrRd='3px' Width='90px' Height='40px' />
                         </div>
                     </div>
                     <div className='col-8'>
-                        <Image URL="https://picsum.photos/1080/1920" Width="1230px" Height="690px" />
+                        <Image URL={images.img} Width="1230px" Height="690px" />
                     </div>
                 </div>
             </div>
