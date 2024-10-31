7aed314a3e638dbc7aeea3c1c986efed2cc138d3
Berk Akın <berkakin99@hotmail.com>
2024-10-31 21:01:40 +0300

    STYLE & REFACTOR: InfoContainer information is now representing in this container

--- frontend/src/Components/LandingContainer/LandingContainer.tsx
+++ frontend/src/Components/LandingContainer/LandingContainer.tsx
@@ -7,14 +7,22 @@ import images from '../../assets/images/LandingImages/images';
 function LandingContainer() {
     return (
         <div>
-            <div className='container-fluid mt-5'>
+            <div className='container-fluid mt-3'>
                 <div className='row'>
                     <div className='col-4 d-flex justify-content-center flex-column'>
-                        <div className='row ps-3 '>
+                        <div className='row ps-5 my-4'>
                             <h1 id={styles["landingText"]}>Her şey dahil araç abonelik sistemi</h1>
                         </div>
-                        <div className='row mt-5 align-items-end h-25 d-flex justify-content-center '>
-                            <Button Text='Detay' BgColor='#E00000' Color='#FFFFFF' Border="none" CrRd='3px' Width='90px' Height='40px' />
+                        <div className='row ps-5 mt-2 mb-4'>
+                            <h6 className={styles.textContent}>
+                                Araç abonelik sistemi, sizi karmaşık prosedürlerle boğulmaktan kurtararak özgürlüğün tadını çıkarmanızı
+                                sağlar. Sadece istediğiniz araç modelini seçin ve her şey dahil paketlerimizden birini tercih edin. Aracınız kapınıza kadar teslim edilsin,
+                                anahtarları alın ve sadece sürmeye başlayın. Geri kalan tüm detaylar ve işlemler bizim sorumluluğumuzda. Siz sadece sürün, gerisini biz
+                                halledelim!
+                            </h6>
+                        </div>
+                        <div className='row justify-content-center my-5'>
+                            <Button Text='Detay' BgColor='#E00000' Color='#FFFFFF' Border="none" CrRd='3px' Width='200px' Height='40px' />
                         </div>
                     </div>
                     <div className='col-8'>
