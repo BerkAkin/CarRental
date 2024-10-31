53789a0b1f74619549ee9c16516dabab4b2b3497
Berk Akın <berkakin99@hotmail.com>
2024-10-31 20:14:24 +0300

    FEAT:Created Image Component

--- frontend/src/Components/Image/Image.tsx
+++ frontend/src/Components/Image/Image.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from "./styles.module.css"
 
-interface ImageParams{
-    URL: string;
-    Width : string;
-    Height : string;
-    Alt? : string;
+interface ImageParams {
+  URL: string;
+  Width: string;
+  Height: string;
+  Alt?: string;
 }
 
-function Image({URL, Width, Height, Alt}: ImageParams) {
+function Image({ URL, Width, Height, Alt }: ImageParams) {
   return (
     <>
-    <img src={URL} width={Width} height={Height} alt={Alt}></img>
+      <img src={URL} width={Width} height={Height} alt={Alt}></img>
     </>
   )
 }
