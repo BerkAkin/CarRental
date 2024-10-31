5328b35e4469ba181dd387c2828f01866d016f8b
Berk Akın <berkakin99@hotmail.com>
2024-10-31 21:01:17 +0300

    REMOVE: InfoContainer

--- frontend/src/Components/InnerContainer/InnerContainer.tsx
+++ frontend/src/Components/InnerContainer/InnerContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import style from './styles.module.css'
 import LandingContainer from '../LandingContainer/LandingContainer'
-import InfoContainer from '../InfoContainer/InfoContainer'
 import WhyInfo from '../WhyInfo/WhyInfo'
 import images from '../../assets/images/WhyImages/images';
 
@@ -10,8 +9,6 @@ function InnerContainer() {
   return (
     <div className={`${style.innerContainerSizing} pt-5`}>
       <LandingContainer />
-      <InfoContainer />
-
       <WhyInfo ImgURL={images.img1} Header='Neden Flexper ?' InfoBars={[
         { HTitle: "Esnek Paketler", PTitle: "6 ve 9 aylık esnek abonelik paketlerimiz ile dilediğiniz zaman aracınızı ve abonelik paketinizi değiştirebilirsiniz. 2 yıllık bağlayıcı sözleşmelere veda edin!" },
         { HTitle: "Müşteri Memnuniyeti", PTitle: "Müşteri merkezli hizmet politikamız ile %100 müşteri memnuniyeti sunuyoruz. Her müşterimize özel birebir hizmet!" },
