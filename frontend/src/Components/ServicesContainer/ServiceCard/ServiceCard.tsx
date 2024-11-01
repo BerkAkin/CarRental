9333decf09cf050b5f4ba1bca83155a6c741495c
Berk Akın <berkakin99@hotmail.com>
2024-11-01 17:13:54 +0300

    FEAT: Single service card component implemented

---
+++ frontend/src/Components/ServicesContainer/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './styles.module.css'
+import Paragraph from '../../WhyInfo/InfoBar/Paragraph/Paragraph';
+import Header from '../../WhyInfo/InfoBar/Header/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+interface ServiceCardProps {
+    HeaderTxt: string;
+    ParagraphTxt: string;
+    Icon: IconProp;
+}
+
+function ServiceCard({ HeaderTxt, ParagraphTxt, Icon }: ServiceCardProps) {
+    return (
+        <>
+            <div className='my-4'>
+                <div className='h-25 justify-content-center row'><FontAwesomeIcon fontSize={50} icon={Icon} /></div>
+                <div className='h-25 my-2 row'><Header Align="center" Text={HeaderTxt} /></div>
+                <div className='h-50 row'><Paragraph Align="center" Text={ParagraphTxt} /></div>
+            </div>
+        </>
+    )
+}
+
+export default ServiceCard
