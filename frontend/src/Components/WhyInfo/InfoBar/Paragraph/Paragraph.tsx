5297116e057d14358122ace54e50857264ab319e
Berk Akın <berkakin99@hotmail.com>
2024-11-01 17:14:54 +0300

    REFACTOR & FEAT: Paragraph component is now asking for Icon and codebase improved for readability

--- frontend/src/Components/WhyInfo/InfoBar/Paragraph/Paragraph.tsx
+++ frontend/src/Components/WhyInfo/InfoBar/Paragraph/Paragraph.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import style from './styles.module.css'
+import React from 'react';
+import style from './styles.module.css';
 
 interface ParagraphProps {
     Text: string;
-    Align: boolean;
+    Align?: boolean | 'center';
 }
 
 function Paragraph({ Text, Align }: ParagraphProps) {
     const props = {
-        "--why-paragraph-align": Align ? 'left' : 'right',
+        "--why-paragraph-align": Align === 'center' ? 'center' : Align ? 'left' : 'right',
     } as React.CSSProperties;
 
     return (
-        <>
-            <p className={style.paragraphOpt} style={props}>
-                {Text}
-            </p>
-        </>
-    )
+        <p className={style.paragraphOpt} style={props}>
+            {Text}
+        </p>
+    );
 }
 
-export default Paragraph
+export default Paragraph;
