870aeb1f8e6459abff3ea87e629f1b08720373bc
Berk Akın <berkakin99@hotmail.com>
2024-11-01 20:34:38 +0300

    FIX: isBold and isHover options removed from ListElement component definition due to unnecessary checks

--- frontend/src/Components/Navbar/TopNav/TopNav.tsx
+++ frontend/src/Components/Navbar/TopNav/TopNav.tsx
@@ -9,11 +9,11 @@ function TopNav() {
         <div className="container pt-2 ms-5">
           <div className="row pt-1 ">
             <ul className="ms-2 d-flex">
-              <ListElement text="info@flexper.com.tr" fs="0.8em" boldness="500" isHref={true} href="mailto:info@flexper.com.tr" color="white" isBold = {true} isHover = {false}/>
+              <ListElement text="info@flexper.com.tr" fs="0.8em" boldness="500" isHref={true} href="mailto:info@flexper.com.tr" color="white" isHover={false} />
               <div className="vr ms-4 me-3"></div>
-              <ListElement text=" +90 543 353 97 37" fs="0.8em" boldness="500" isHref={false} href="" color="white" isBold={true} isHover={false}/>
+              <ListElement text=" +90 543 353 97 37" fs="0.8em" boldness="500" isHref={false} href="" color="white" isHover={false} />
               <div className="vr ms-3 me-3"></div>
-              <ListElement text=" WhatsApp" fs="0.8em" boldness="500" isHref={false} href="" color="white"  isBold={true} isHover={false} />
+              <ListElement text=" WhatsApp" fs="0.8em" boldness="500" isHref={false} href="" color="white" isHover={false} />
             </ul>
           </div>
         </div>
