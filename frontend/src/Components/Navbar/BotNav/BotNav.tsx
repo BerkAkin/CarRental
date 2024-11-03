692be5d54143fc3d5525de805f45431c87a0cfb5
Berk Akın <berkakin99@hotmail.com>
2024-11-03 20:29:53 +0300

    STYLE && REFACTOR: Removed extra spaces between navbar elements and removed unnecessary html nodes and stylings

--- frontend/src/Components/Navbar/BotNav/BotNav.tsx
+++ frontend/src/Components/Navbar/BotNav/BotNav.tsx
@@ -9,30 +9,21 @@ function BotNav() {
     <>
       <div className={`${styles.botNav} ps-2 container-fluid`}>
         <div className='ms-5 h-100 col-6 '>
-          <div className=' row h-100 d-flex container-fluid  '>
+          <div className='row h-100 d-flex container-fluid '>
             <div className='col-2 align-items-center d-flex  ps-0'>
               <Image URL={flexperLogo} Width="160" Height="40" />
             </div>
-            <div className='col-9 p-0 d-flex ms-4'>
-              <div className='col d-flex align-items-center justify-content-center'>
-                <ListElement color="#1A2B48" text='ANASAYFA' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              </div>
-              <div className='col d-flex align-items-center justify-content-center'>
-                <ListElement color="#1A2B48" text='MODELLER' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              </div>
-              <div className='col-3 d-flex align-items-center justify-content-center'>
-                <ListElement color="#1A2B48" text='SIK SORULAN SORULAR' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              </div>
-              <div className='col d-flex align-items-center justify-content-center'>
-                <ListElement color="#1A2B48" text='BİZ KİMİZ?' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              </div>
-              <div className='col d-flex align-items-center justify-content-center'>
-                <ListElement color="#1A2B48" text='İLETİŞİM' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              </div>
+            <div className='col-10 d-flex align-items-center justify-content-evenly  text-center'>
+              <ListElement color="#1A2B48" text='ANASAYFA' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='MODELLER' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='BİZ KİMİZ?' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='SIK SORULAN SORULAR' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='İLETİŞİM' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='BLOG' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
             </div>
           </div>
         </div>
-      </div>
+      </div >
     </>
   )
 }
