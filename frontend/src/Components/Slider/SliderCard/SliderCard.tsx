1fc439521b0e3c59f6bc0c40dfc74324df18a514
Berk Akın <berkakin99@hotmail.com>
2024-11-05 02:58:12 +0300

    STYLE: Removed unnecessary classes

--- frontend/src/Components/Slider/SliderCard/SliderCard.tsx
+++ frontend/src/Components/Slider/SliderCard/SliderCard.tsx
@@ -11,11 +11,11 @@ function SliderCard({ Comment, StarCount, Person, Type }: SliderCardProps) {
     return (
         <>
             <div className='col'>
-                <div className={`${styles.cardBg} h-100 d-flex flex-column rounded`}>
-                    <div className='h-75 text-start p-4 '>
+                <div className={`${styles.cardBg} d-flex justify-content-between flex-column rounded`}>
+                    <div className='text-start p-4'>
                         <p>{Comment}</p>
                     </div>
-                    <div className='ps-4 mb-3'>
+                    <div className='ps-4 pb-3'>
                         <section className='fs-6'>{StarCount}</section>
                         <section className={styles.textFeatures}>{Person}</section>
                         <section className={styles.textColor}>{Type}</section>
