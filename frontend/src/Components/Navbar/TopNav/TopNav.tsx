6b554f7a3ce4c98bf6f6862745a1d4d37ab052ef
Berk Akın <berkakin99@hotmail.com>
2024-11-07 20:41:04 +0300

    FIX: First element of navbar which is mail is now changed to a element to fix React Router Dom components mailto href issue

--- frontend/src/Components/Navbar/TopNav/TopNav.tsx
+++ frontend/src/Components/Navbar/TopNav/TopNav.tsx
@@ -9,7 +9,7 @@ function TopNav() {
         <div className="container pt-2 ms-5">
           <div className="row pt-1 ">
             <ul className="ms-2 d-flex">
-              <ListElement text="info@flexper.com.tr" fs="0.8em" boldness="500" isHref={true} href="mailto:info@flexper.com.tr" color="white" isHover={false} />
+              <a href="mailto:info@flexper.com.tr" style={{ color: "white", textDecoration: "none", fontSize: "0.8em", marginTop: "2px" }}>info@flexper.com.tr</a>
               <div className="vr ms-4 me-3"></div>
               <ListElement text=" +90 543 353 97 37" fs="0.8em" boldness="500" isHref={false} href="" color="white" isHover={false} />
               <div className="vr ms-3 me-3"></div>
