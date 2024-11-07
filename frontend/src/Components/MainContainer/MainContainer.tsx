78e66971db7757a30637af046823ec938b40d810
Berk Akın <berkakin99@hotmail.com>
2024-11-07 20:40:08 +0300

    FEAT: Routes of pages defined

--- frontend/src/Components/MainContainer/MainContainer.tsx
+++ frontend/src/Components/MainContainer/MainContainer.tsx
@@ -2,17 +2,35 @@ import React from "react";
 import "../../app.css";
 import TopNav from "../Navbar/TopNav/TopNav";
 import BotNav from "../Navbar/BotNav/BotNav";
-import InnerContainer from "../Containers/LandingContainer/LandingContainer";
+import LandingContainer from "../Containers/LandingContainer/LandingContainer";
 import Footer from "../Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ModelsContainer from "../Containers/ModelsContainer/ModelsContainer";
+import AboutContainer from "../Containers/AboutContainer/AboutContainer";
+import FAQContainer from "../Containers/FAQContainer/FAQContainer";
+import ContactContainer from "../Containers/ContactContainer/ContactContainer";
+import BlogContainer from "../Containers/BlogContainer/BlogContainer";
+import DetailContainer from "../Containers/DetailContainer/DetailContainer";
 
 function MainContainer() {
   return (
-    <div>
-      <TopNav />
-      <BotNav />
-      <InnerContainer />
-      <Footer />
-    </div>
+    <>
+      <Router>
+        <TopNav />
+        <BotNav />
+        <Routes>
+          <Route path="/" element={<LandingContainer />}></Route>
+          <Route path="/Models" element={<ModelsContainer />}></Route>
+          <Route path="/Models/:id" element={<DetailContainer />} />
+          <Route path="/About" element={<AboutContainer />}></Route>
+          <Route path="/FAQ" element={<FAQContainer />}></Route>
+          <Route path="/Contact" element={<ContactContainer />}></Route>
+          <Route path="/Blog" element={<BlogContainer />}></Route>
+        </Routes>
+
+        <Footer />
+      </Router>
+    </>
   );
 }
 
