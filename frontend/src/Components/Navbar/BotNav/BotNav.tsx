a72ff91fac273e88e0baa86ba2988360dd3744b6
Berk Akın <berkakin99@hotmail.com>
2024-11-11 00:27:10 +0300

    STYLE: Changed navbar links order

--- frontend/src/Components/Navbar/BotNav/BotNav.tsx
+++ frontend/src/Components/Navbar/BotNav/BotNav.tsx
@@ -16,10 +16,10 @@ function BotNav() {
             <div className='col-10 d-flex align-items-center justify-content-evenly  text-center'>
               <ListElement color="#1A2B48" text='ANASAYFA' href='' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
               <ListElement color="#1A2B48" text='MODELLER' href='Models' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              <ListElement color="#1A2B48" text='BİZ KİMİZ?' href='About' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='FLEXBLOG' href='Flexblog' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
               <ListElement color="#1A2B48" text='SIK SORULAN SORULAR' href='FAQ' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+              <ListElement color="#1A2B48" text='BİZ KİMİZ?' href='About' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
               <ListElement color="#1A2B48" text='İLETİŞİM' href='Contact' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
-              <ListElement color="#1A2B48" text='BLOG' href='Blog' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
             </div>
           </div>
         </div>
