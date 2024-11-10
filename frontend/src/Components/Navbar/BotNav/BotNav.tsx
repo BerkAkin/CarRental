89c83ddbca843841f0ae6a89cd45e17424880171
Berk Akın <berkakin99@hotmail.com>
2024-11-11 01:01:38 +0300

    FEAT: Added login and register buttons

--- frontend/src/Components/Navbar/BotNav/BotNav.tsx
+++ frontend/src/Components/Navbar/BotNav/BotNav.tsx
@@ -7,8 +7,8 @@ import flexperLogo from '../../../assets/logos/logo-flexper.png';
 function BotNav() {
   return (
     <>
-      <div className={`${styles.botNav} ps-2 container-fluid`}>
-        <div className='ms-5 h-100 col-6 '>
+      <div className={`${styles.botNav} ps-2 d-flex justify-content-between container-fluid`}>
+        <div className='ms-5 h-100 col-6'>
           <div className='row h-100 d-flex container-fluid '>
             <div className='col-2 align-items-center d-flex  ps-0'>
               <Image URL={flexperLogo} Width="160" Height="40" />
@@ -20,6 +20,14 @@ function BotNav() {
               <ListElement color="#1A2B48" text='SIK SORULAN SORULAR' href='FAQ' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
               <ListElement color="#1A2B48" text='BİZ KİMİZ?' href='About' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
               <ListElement color="#1A2B48" text='İLETİŞİM' href='Contact' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} />
+            </div>
+          </div>
+        </div>
+        <div className='col-5'>
+          <div className='container h-100 d-flex justify-content-end'>
+            <div className='row w-100 justify-content-end  text-center'>
+              <div className='col-2 align-items-center d-flex justify-content-center'> <ListElement color="#1A2B48" text='ÜYE OL' href='login' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} /></div>
+              <div className='col-2 align-items-center d-flex justify-content-center'> <ListElement color="#1A2B48" text='GİRİŞ YAP' href='register' boldness='700' fs='0.9em' isHover={true} hoverColor='#E00000' isHref={true} /></div>
             </div>
           </div>
         </div>
