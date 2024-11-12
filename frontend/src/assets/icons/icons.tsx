9941b3b7ea27f83aac87645f32e857a52ff66107
Berk Akın <berkakin99@hotmail.com>
2024-11-12 20:16:35 +0300

    STYLE: Icons on modelCard imported

---
+++ frontend/src/assets/icons/icons.tsx
@@ -0,0 +1,8 @@
+import CarDoorIcon from './CarDoor.png';
+import LuggageIcon from './Luggage.png'
+import PersonIcon from './Person.png'
+import GearIcon from './Gear.png'
+
+export default {
+    CarDoorIcon, LuggageIcon, PersonIcon, GearIcon
+}
