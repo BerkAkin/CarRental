f281cfc522a3e0e47274382dce6bc56a9cb78426
Berk Akın <berkakin99@hotmail.com>
2024-11-14 04:16:17 +0300

    FEAT & STYLE: detailpage implemented and styled

--- frontend/src/Components/Containers/DetailContainer/DetailContainer.tsx
+++ frontend/src/Components/Containers/DetailContainer/DetailContainer.tsx
@@ -7,6 +7,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faTag } from '@fortawesome/free-solid-svg-icons'
 import styles from './styles.module.css'
 import Icons from '../../../assets/icons/icons';
+import sliderModels from '../../../common/sliderModels';
+import SimpleSlider from '../../SimpleSlider/SimpleSlider';
+import SliderModelCard from '../../SliderModelCard/SliderModelCard';
 
 function DetailContainer() {
 
@@ -18,111 +21,188 @@ function DetailContainer() {
 
     return (
         <>
-            <div className='container mt-5 pt-5'>
+            <div className='container my-5 py-5'>
                 <div className='row'>
-                    <div className='col-12 ps-5'>
+                    <div className='col-12'>
                         <h2>Dizel – Manuel Ford Tourneo Courier</h2> {/* Dinamik */}
-                        <p className='mt-3'> Aylık <span className='fw-bold'>32.900 TL + kdv </span> ‘den başlayan fiyatlar ile sizlerle!
+                        <p style={{ color: "#7A7A7A" }} className='mt-3 '> Aylık <span className='fw-bold'>32.900 TL + kdv </span> ‘den başlayan fiyatlar ile sizlerle!
                             Üstelik aylık sadece 699 TL +kdv ile Aile Paketine üye olabilir ve aile bireylerinizi ek kullanıcı olarak ekleyebilirsiniz!</p>
                     </div>
+
                 </div>
 
+
                 <div className='row mt-4'>
-                    <div className='col-6 '>
+                    <div className='col-6 d-flex align-items-center'>
                         <Image URL={dummy} Width='600' />{/* Dinamik */}
                     </div>
 
-                    <div className='col-3 '>
-
-                        <div className='row text-center'>
-                            <h3 className='ps-4'>Marka</h3>
-                        </div>
-
-                        <hr className='mt-2' />
-                        <div className='container mt-3 text-center '>
-                            <div className='row'>
-                                <div className='col-3 d-flex justify-content-end align-items-center'>
-                                    <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faTag}></FontAwesomeIcon>
+                    <div className='col-6 '>
+                        <div className='row h-25'>
+                            <div className='col-6'>
+                                <div className='row'>
+                                    <h3>Marka</h3>
                                 </div>
-                                <div className='col-9 ps-0 d-flex'>
-                                    <span className={styles.feats}>Ford</span> {/* Dinamik */}
+                                <div className='row'>
+                                    <div className='container mt-1'>
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faTag}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2 '>
+                                                <p className={styles.feats}>Ford</p> {/* Dinamik */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-6'>
+                                <div className='row'>
+                                    <h3>Kapasite Bilgisi</h3>
+                                </div>
+                                <div className='row'>
+                                    <div className='container mt-1'>
+                                        <div className='row'>
+                                            <div className='col-2 d-flex justify-content-center'>
+                                                <img className={`${styles.IconSize}`} src={Icons.PersonIcon}></img>
+                                            </div>
+                                            <div className='col-2 d-flex justify-content-center'>
+                                                <img className={`${styles.IconSize}`} src={Icons.LuggageIcon}></img>
+                                            </div>
+                                            <div className='col-2 d-flex justify-content-center '>
+                                                <img className={`${styles.IconSize}`} src={Icons.CarDoorIcon}></img>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-2 d-flex justify-content-center '>
+                                                {/* Dinamik */} <p>4</p>
+                                            </div>
+                                            <div className='col-2 d-flex justify-content-center'>
+                                                {/* Dinamik */}<p>4</p>
+                                            </div>
+                                            <div className='col-2 d-flex justify-content-center'>
+                                                {/* Dinamik */} <p>4</p>
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-
-                        <div className='row mt-5 text-center'>
-                            <h3 className='ps-4'>Kapasite Bilgisi</h3>
-
-                        </div>
-                        <hr className='mt-2' />
-                        <div className='container mt-3 text-center '>
-                            <div className='row'>
-
-                                <div className='col-3 d-flex justify-content-end align-items-center'>
-                                    <div className=''><img className={`${styles.IconSize}`} src={Icons.PersonIcon}></img></div>
+                        <div className='row h-75 pt-3'>
+                            <div className='col-6 '>
+                                <div className='row'>
+                                    <h3>Araç Özellikleri</h3>
                                 </div>
-                                <div className='col-9 ps-0 d-flex'>
-                                    {/* Dinamik Dolacak */}
+                                <div className='row'>
+                                    <div className='container mt-1'>
+                                        {/* foreach ile dönülecek ksıım burası */}
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={`${styles.feats}`}>1499 cm³ Motor Hacmi</p>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={`${styles.feats}`}>1499 cm³ Motor Hacmi</p>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={`${styles.feats}`}>1499 cm³ Motor Hacmi</p>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={`${styles.feats}`}>1499 cm³ Motor Hacmi</p>
+                                            </div>
+                                        </div>
+                                        <div className='row'>
+                                            <div className='col-1'>
+                                                <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={`${styles.feats}`}>1499 cm³ Motor Hacmi</p>
+                                            </div>
+                                        </div>
+
+                                        <p>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className='row mt-4'>
-                                <div className='col-3 d-flex justify-content-end align-items-center'>
-                                    <div className=''> <img className={`${styles.IconSize}`} src={Icons.LuggageIcon}></img></div>
+                            <div className='col-6'>
+                                <div className='row'>
+                                    <h3> Diğer Hizmetler</h3>
                                 </div>
-                                <div className='col-9 ps-0 d-flex'>
-                                    {/* Dinamik Dolacak */}
+                                <div className='row'>
+                                    <div className='container mt-1'>
+                                        <div className='row'>
+                                            {/* foreach ile dönülecek */}
+                                            <div className='col-1'>
+                                                <GreenCheck width="15" height="15" />
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={styles.feats}>Otopark Hizmeti</p>
+                                            </div>
+                                            <div className='col-1'>
+                                                <GreenCheck width="15" height="15" />
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={styles.feats}>Otopark Hizmeti</p>
+                                            </div>
+                                            <div className='col-1'>
+                                                <GreenCheck width="15" height="15" />
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={styles.feats}>Otopark Hizmeti</p>
+                                            </div>
+                                            <div className='col-1'>
+                                                <GreenCheck width="15" height="15" />
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={styles.feats}>Otopark Hizmeti</p>
+                                            </div>
+                                            <div className='col-1'>
+                                                <GreenCheck width="15" height="15" />
+                                            </div>
+                                            <div className='col-11 ps-2'>
+                                                <p className={styles.feats}>Otopark Hizmeti</p>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className='row mt-4'>
-                                <div className='col-3 d-flex justify-content-end align-items-center'>
-                                    <div className=''><img className={`${styles.IconSize}`} src={Icons.CarDoorIcon}></img></div>
-                                </div>
-                                <div className='col-9 ps-0 d-flex'>
-                                    {/* Dinamik Dolacak */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div className='col-3 '>
-                        <div className='row text-center'>
-                            <h3 className='ps-4'>Araç Özellikleri</h3>
-                        </div>
-                        <hr className='mt-2' />
-                        <div className='container mt-1 text-center '>
-
-                            {/* foreach ile dönülecek ksıım burası */}
-                            <div className='row'>
-                                <div className='col-3 d-flex justify-content-end align-items-center'>
-                                    <FontAwesomeIcon style={{ color: "#1A2B48" }} icon={faStar}></FontAwesomeIcon>
-                                </div>
-                                <div className='col-9 ps-0 d-flex'>
-                                    <span className={`${styles.feats}`}>1499 cm³ Motor Hacmi</span>
-                                </div>
-                            </div>
-
-                            <p>
-                            </p>
                         </div>
                     </div>
                 </div>
-
-                <div className='row pt-3 mt-3'>
-                    <div className='container'>
-                        <h3 className='text-center'>Bu Araçla Birlikte Sunulan Diğer Hizmetler</h3>  <hr />
-                    </div>
-
-                    {/* foreach ile dönülecek */}
-                    <div className='col-3 mt-3 justify-content-center d-flex'>
-                        <p><GreenCheck width="20" height="20" /><span className='ms-2 fw-bold'>Otopark Hizmeti</span></p>
-                    </div>
-                </div>
-
             </div >
+            <div className='mt-5 pt-5'>
+                <div className='mt-5'>
+                    <SimpleSlider slidesToShow={4} header='Diğer Modeller' items={sliderModels} renderItem={(item) => (
+
+                        <SliderModelCard type={item.Type} id={item.id} brandName={item.Brand} doorCount={item.DoorCount} gear={item.Gear} image={item.imgURL} luggageCount={item.LuggageCount} personCount={item.PeopleCount} price={item.Price} />
+                    )} />
+                </div>
+
+            </div>
+
+
         </>
     )
 }
