0754aa02e60beaa7283aea0ae3e52c6bbf86280e
Berk Akın <berkakin99@hotmail.com>
2024-11-14 04:12:45 +0300

    REFACTOR: Slider models are now separated file until backend development

---
+++ frontend/src/common/sliderModels.tsx
@@ -0,0 +1,38 @@
+
+const models = [
+    {
+        id: 1,
+        imgURL: "https://picsum.photos/1000",
+        Type: "SUV",
+        Brand: "Ford Puma Style",
+        PeopleCount: 4,
+        Gear: "Auto",
+        LuggageCount: 3,
+        DoorCount: 4,
+        Price: "39,000"
+    },
+    {
+        id: 2,
+        imgURL: "https://picsum.photos/1000",
+        Type: "SUV",
+        Brand: "Ford Puma Style",
+        PeopleCount: 4,
+        Gear: "Auto",
+        LuggageCount: 3,
+        DoorCount: 4,
+        Price: "39,000"
+    },
+    {
+        id: 3,
+        imgURL: "https://picsum.photos/1000",
+        Type: "SUV",
+        Brand: "Ford Puma Style",
+        PeopleCount: 4,
+        Gear: "Auto",
+        LuggageCount: 3,
+        DoorCount: 4,
+        Price: "39,000"
+    },
+]
+
+export default models;
