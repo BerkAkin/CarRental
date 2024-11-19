8cde62a32e380f5d5a7f403a3ca45dda8c8cf6aa
Berk Akın <berkakin99@hotmail.com>
2024-11-19 16:56:56 +0300

    FEAT: GetById function body defined

--- backend/WebApi/Repositories/IRepository.cs
+++ backend/WebApi/Repositories/IRepository.cs
@@ -4,8 +4,8 @@ namespace WebApi.Repository
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        /* Task<T> GetByIdAsync(int id);
-        Task AddAsync(T entity);
+        Task<T> GetByIdAsync(int id);
+        /* Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id); */
     }
