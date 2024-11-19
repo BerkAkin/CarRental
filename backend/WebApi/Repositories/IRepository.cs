78269022f0fc5a1eeb52867d62efc0a4ef1d9905
Berk Akın <berkakin99@hotmail.com>
2024-11-20 01:03:10 +0300

    FIX: Repository interface is now including delete and add method bodies

--- backend/WebApi/Repositories/IRepository.cs
+++ backend/WebApi/Repositories/IRepository.cs
@@ -6,8 +6,7 @@ namespace WebApi.Repository
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task UpdateAsync(T entity);
-        /* Task AddAsync(T entity);
-        
-        Task DeleteAsync(int id); */
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
