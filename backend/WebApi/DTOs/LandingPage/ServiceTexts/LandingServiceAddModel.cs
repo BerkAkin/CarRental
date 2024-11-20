0bc9e4657bc35bab86dfd98d74afcfc09eac9057
Berk Akın <berkakin99@hotmail.com>
2024-11-20 22:57:26 +0300

    FEAT: ServicesText Adder DTO Model implemented

---
+++ backend/WebApi/DTOs/LandingPage/ServiceTexts/LandingServiceAddModel.cs
@@ -0,0 +1,9 @@
+namespace WebApi.DTOs.LandingPage.ServiceTexts
+{
+    public class LandingServiceAddModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Icon { get; set; }
+    }
+}
