f5256470a05894803a86e838092a33010c62bfbc
Berk Akın <berkakin99@hotmail.com>
2024-11-23 18:15:12 +0300

    REFACTOR: Repo is now inheriting from BaseRepository class

--- backend/WebApi/Repositories/LandingRepositories/MainTextRepository.cs
+++ backend/WebApi/Repositories/LandingRepositories/MainTextRepository.cs
@@ -1,19 +1,11 @@
-using Microsoft.EntityFrameworkCore;
 using WebApi.DbOperations;
 using WebApi.Entities;
-using WebApi.Repository;
 
 namespace WebApi.Repositories.LandingRepositories
 {
-    public class MainTextRepository : IRepository<LandingMainText>
+    public class MainTextRepository : BaseRepository<LandingMainText>
     {
-        private readonly CRDbContext _context;
-
-        public MainTextRepository(CRDbContext context)
-        {
-            _context = context;
-        }
-
+        public MainTextRepository(CRDbContext context) : base(context) { }
         public Task AddAsync(LandingMainText entity)
         {
             throw new NotImplementedException();
@@ -23,23 +15,5 @@ namespace WebApi.Repositories.LandingRepositories
         {
             throw new NotImplementedException();
         }
-
-        public async Task<IEnumerable<LandingMainText>> GetAllAsync()
-        {
-            return await _context.LandingMainTexts.ToListAsync();
-        }
-
-        public async Task<LandingMainText> GetByIdAsync(int id)
-        {
-            return await _context.LandingMainTexts.FindAsync(id);
-        }
-
-        public async Task UpdateAsync(LandingMainText entity)
-        {
-            _context.LandingMainTexts.Update(entity);
-            await _context.SaveChangesAsync();
-        }
-
-
     }
 }
