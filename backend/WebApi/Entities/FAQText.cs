b45c952113f7ecb2852bd83c4bfb03b6d47a749b
Berk Akın <berkakin99@hotmail.com>
2024-11-23 17:48:18 +0300

    FEAT: FAQ entity defined

---
+++ backend/WebApi/Entities/FAQText.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Entities
+{
+    public class FAQText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
