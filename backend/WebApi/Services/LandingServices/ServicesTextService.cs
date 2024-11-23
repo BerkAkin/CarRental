4f323cac9350c4215bad0f305d6b9c51054f8553
Berk Akın <berkakin99@hotmail.com>
2024-11-23 17:52:36 +0300

    REFACTOR: The service has now been refactored to inherit from the baseservice.

--- backend/WebApi/Services/LandingServices/ServicesTextService.cs
+++ backend/WebApi/Services/LandingServices/ServicesTextService.cs
@@ -5,73 +5,10 @@ using WebApi.Repository;
 
 namespace WebApi.Services.LandingServices
 {
-    public class ServicesTextService
+    public class ServicesTextService : BaseService<LandingServiceText, LandingServiceAddModel, LandingServiceUpdateModel, LandingServiceViewModel, LandingServiceViewIdModel>
     {
-        private readonly IRepository<LandingServiceText> _repository;
-        private readonly IMapper _mapper;
-        public ServicesTextService(IRepository<LandingServiceText> repository, IMapper mapper)
+        public ServicesTextService(IRepository<LandingServiceText> repository, IMapper mapper) : base(repository, mapper)
         {
-            _mapper = mapper;
-            _repository = repository;
-        }
-
-        public async Task<List<LandingServiceViewModel>> GetAllAsync()
-        {
-            var landingServiceTexts = await _repository.GetAllAsync();
-            if (landingServiceTexts is null)
-            {
-                throw new InvalidOperationException("Servis Yazıları Bulunamadı");
-            }
-            var landingServiceDTO = _mapper.Map<List<LandingServiceViewModel>>(landingServiceTexts);
-            return landingServiceDTO;
-        }
-
-        public async Task<LandingServiceViewIdModel> GetByIdAsync(int id)
-        {
-            var landingServiceText = await _repository.GetByIdAsync(id);
-            if (landingServiceText is null)
-            {
-                throw new InvalidOperationException("Servis Yazısı Bulunamadı");
-            }
-            var landingServiceTextDTO = _mapper.Map<LandingServiceViewIdModel>(landingServiceText);
-            return landingServiceTextDTO;
-        }
-
-
-
-        public async Task UpdateAsync(int id, LandingServiceUpdateModel model)
-        {
-            var landingServiceText = await _repository.GetByIdAsync(id);
-            if (landingServiceText is null)
-            {
-                throw new ArgumentNullException("Güncellenecek Servis Yazısı Bulunamadı");
-            }
-            _mapper.Map(model, landingServiceText);
-            await _repository.UpdateAsync(landingServiceText);
-        }
-
-
-
-        public async Task AddAsync(LandingServiceAddModel model)
-        {
-            if (model is null)
-            {
-                throw new ArgumentNullException("Eklencek Veri Boş Olamaz");
-            }
-            var entity = _mapper.Map<LandingServiceText>(model);
-            await _repository.AddAsync(entity);
-        }
-
-
-        public async Task DeleteAsync(int id)
-        {
-            var entity = await _repository.GetByIdAsync(id);
-            if (entity is null)
-            {
-                throw new KeyNotFoundException("Silinecek Servis Yazısı Bulunamadı");
-
-            }
-            await _repository.DeleteAsync(entity);
         }
     }
 }
