935dabec677145b352479fb55478090ec0f6f122
Berk Akın <berkakin99@hotmail.com>
2024-11-23 17:49:15 +0300

    Assembly Cache Update

--- backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6f91ec7a0051cb53a219386209e4fca1bbca4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068483b773dfb6948daf898f7778320e6dd7a448")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
