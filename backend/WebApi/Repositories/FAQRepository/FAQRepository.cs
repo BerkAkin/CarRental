c95e5f0fdcc32f471233f6238ff4e39758af0db2
Berk Akın <berkakin99@hotmail.com>
2024-11-23 18:15:09 +0300

    REFACTOR: Repo is now inheriting from BaseRepository class

--- backend/WebApi/Repositories/FAQRepository/FAQRepository.cs
+++ backend/WebApi/Repositories/FAQRepository/FAQRepository.cs
@@ -1,44 +1,10 @@
-using Microsoft.EntityFrameworkCore;
 using WebApi.DbOperations;
 using WebApi.Entities;
-using WebApi.Repository;
 
 namespace WebApi.Repositories.FAQRepository
 {
-    public class FAQRepository : IRepository<FAQText>
+    public class FAQRepository : BaseRepository<FAQText>
     {
-        private readonly CRDbContext _context;
-        public FAQRepository(CRDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IEnumerable<FAQText>> GetAllAsync()
-        {
-            return await _context.FAQTexts.ToListAsync();
-        }
-
-        public async Task<FAQText> GetByIdAsync(int id)
-        {
-            return await _context.FAQTexts.FindAsync(id);
-        }
-
-        public async Task AddAsync(FAQText entity)
-        {
-            _context.FAQTexts.Add(entity);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(FAQText entity)
-        {
-            _context.FAQTexts.Update(entity);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(FAQText entity)
-        {
-            _context.FAQTexts.Remove(entity);
-            await _context.SaveChangesAsync();
-        }
+        public FAQRepository(CRDbContext context) : base(context) { }
     }
 }
