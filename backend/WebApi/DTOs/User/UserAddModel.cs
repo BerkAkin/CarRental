a805a7f4dfff999a5422906ccd386043508fe111
Berk Akın <berkakin99@hotmail.com>
2024-11-25 17:00:44 +0300

    FEAT: DTO defined

---
+++ backend/WebApi/DTOs/User/UserAddModel.cs
@@ -0,0 +1,13 @@
+using WebApi.Entities;
+
+namespace WebApi.DTOs.User
+{
+    public class UserAddModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
