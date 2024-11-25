b640c91a972117959ec993372272a7d9d42f0e3c
Berk Akın <berkakin99@hotmail.com>
2024-11-25 17:02:53 +0300

    FEAT: User Service defined

--- backend/WebApi/Services/ServiceRegistration.cs
+++ backend/WebApi/Services/ServiceRegistration.cs
@@ -3,9 +3,11 @@ using WebApi.DbOperations;
 using WebApi.Entities;
 using WebApi.Repositories.FAQRepository;
 using WebApi.Repositories.LandingRepositories;
+using WebApi.Repositories.UserRepository;
 using WebApi.Repository;
 using WebApi.Services.FAQService;
 using WebApi.Services.LandingServices;
+using WebApi.Services.UserService;
 using WebApi.Validators.LandingPage.ReasonText;
 
 public static class ServiceRegistration
@@ -23,12 +25,14 @@ public static class ServiceRegistration
         services.AddScoped<IRepository<LandingReasonText>, ReasonTextRepository>();
         services.AddScoped<IRepository<LandingServiceText>, ServiceTextRepository>();
         services.AddScoped<IRepository<FAQText>, FAQRepository>();
+        services.AddScoped<IRepository<User>, UserRepository>();
 
         //SERVICES
         services.AddScoped<MainTextService>();
         services.AddScoped<ReasonTextService>();
         services.AddScoped<ServicesTextService>();
         services.AddScoped<FAQService>();
+        services.AddScoped<UserService>();
 
         //SEEDER
         services.AddTransient<DbSeeder>();
