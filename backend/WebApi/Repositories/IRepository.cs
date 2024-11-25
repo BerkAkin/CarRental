54a80e2dd651220c40858b3b3e1a334608978f16
Berk Akın <berkakin99@hotmail.com>
2024-11-25 17:02:32 +0300

    FEAT: Get and GetById methods are now accepting lambda parameters for including entities if there is a relational datas

--- backend/WebApi/Repositories/IRepository.cs
+++ backend/WebApi/Repositories/IRepository.cs
@@ -3,10 +3,11 @@ namespace WebApi.Repository
 
     public interface IRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync(Func<IQueryable<T>, IQueryable<T>> include = null);
+        Task<T> GetByIdAsync(int id, Func<IQueryable<T>, IQueryable<T>> include = null);
         Task UpdateAsync(T entity);
         Task AddAsync(T entity);
         Task DeleteAsync(T entity);
+        Task SaveAsync();
     }
 }
