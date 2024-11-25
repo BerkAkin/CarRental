5543f9077cc6c848bf77761686a6d858722afe5d
Berk Akın <berkakin99@hotmail.com>
2024-11-25 16:59:49 +0300

    FEAT: Added User, Role and UserComment entites

--- backend/WebApi/DbOperations/CRDbContext.cs
+++ backend/WebApi/DbOperations/CRDbContext.cs
@@ -5,10 +5,22 @@ namespace WebApi.DbOperations
 {
     public class CRDbContext : DbContext
     {
-        public CRDbContext(DbContextOptions<CRDbContext> options) : base(options) { }
+        public CRDbContext(DbContextOptions<CRDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserComment>().HasIndex(uc => uc.UserId).IsUnique();
+        }
+
+
         public DbSet<LandingMainText> LandingMainTexts { get; set; }
         public DbSet<LandingReasonText> LandingReasonTexts { get; set; }
         public DbSet<LandingServiceText> LandingServiceTexts { get; set; }
         public DbSet<FAQText> FAQTexts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserComment> UserComments { get; set; }
     }
 }
