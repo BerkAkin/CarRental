94a0ecbab1c3625e5c97baa2545181750a4ce032
Berk Akın <berkakin99@hotmail.com>
2024-11-25 17:00:56 +0300

    FEAT: Entity defined

---
+++ backend/WebApi/Entities/Role.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
