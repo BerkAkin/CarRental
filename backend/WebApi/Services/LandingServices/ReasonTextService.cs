9715d93cc4855daec4465f224276350189f9fdfc
Berk Akın <berkakin99@hotmail.com>
2024-11-26 20:34:07 +0300

    REFACTOR: IRepository definition changed to service's related repository

--- backend/WebApi/Services/LandingServices/ReasonTextService.cs
+++ backend/WebApi/Services/LandingServices/ReasonTextService.cs
@@ -4,6 +4,7 @@ using FluentValidation.Results;
 using WebApi.DTOs.LandingPage.MainText;
 using WebApi.DTOs.LandingPage.ReasonTexts;
 using WebApi.Entities;
+using WebApi.Repositories.LandingRepositories;
 using WebApi.Repository;
 
 namespace WebApi.Services.LandingServices
@@ -11,7 +12,7 @@ namespace WebApi.Services.LandingServices
     public class ReasonTextService : BaseService<LandingReasonText, LandingReasonAddModel, LandingReasonUpdateModel, LandingReasonViewModel, LandingMainViewIdModel>
     {
 
-        public ReasonTextService(IRepository<LandingReasonText> repository, IMapper mapper) : base(repository, mapper)
+        public ReasonTextService(ReasonTextRepository repository, IMapper mapper) : base(repository, mapper)
         {
         }
 
