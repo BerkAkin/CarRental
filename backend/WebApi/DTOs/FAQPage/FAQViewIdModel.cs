9ee4397a45944ae847cc69ac9060d15e67164e59
Berk Akın <berkakin99@hotmail.com>
2024-11-26 20:28:25 +0300

    FIX: Added Id column to make front-end operations using it column

--- backend/WebApi/DTOs/FAQPage/FAQViewIdModel.cs
+++ backend/WebApi/DTOs/FAQPage/FAQViewIdModel.cs
@@ -2,6 +2,7 @@ namespace WebApi.DTOs.FAQPage
 {
     public class FAQViewIdModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
     }
