24953da2110ef4e437417b3d3499f680cede040d
Berk Akın <berkakin99@hotmail.com>
2024-11-26 20:34:01 +0300

    REFACTOR: IRepository definition changed to service's related repository

--- backend/WebApi/Services/FAQService/FAQService.cs
+++ backend/WebApi/Services/FAQService/FAQService.cs
@@ -1,12 +1,13 @@
 using AutoMapper;
 using WebApi.DTOs.FAQPage;
 using WebApi.Entities;
+using WebApi.Repositories.FAQRepository;
 using WebApi.Repository;
 
 namespace WebApi.Services.FAQService
 {
     public class FAQService : BaseService<FAQText, FAQAddModel, FAQUpdateModel, FAQViewModel, FAQViewIdModel>
     {
-        public FAQService(IRepository<FAQText> repository, IMapper mapper) : base(repository, mapper) { }
+        public FAQService(FAQRepository repository, IMapper mapper) : base(repository, mapper) { }
     }
 }
