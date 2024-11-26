47f44935d3357cfe2cf67aed5e1915a6b9f6902d
Berk Akın <berkakin99@hotmail.com>
2024-11-26 20:28:57 +0300

    FEAT: Added PhoneNum column

--- backend/WebApi/DTOs/User/UserAddModel.cs
+++ backend/WebApi/DTOs/User/UserAddModel.cs
@@ -7,6 +7,7 @@ namespace WebApi.DTOs.User
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        public string PhoneNum { get; set; }
         public string Password { get; set; }
 
     }
