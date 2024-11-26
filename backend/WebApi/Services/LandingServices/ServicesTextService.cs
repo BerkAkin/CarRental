1708ca5143385d437147238e078f80451c463e49
Berk Akın <berkakin99@hotmail.com>
2024-11-26 20:34:09 +0300

    REFACTOR: IRepository definition changed to service's related repository

--- backend/WebApi/Services/LandingServices/ServicesTextService.cs
+++ backend/WebApi/Services/LandingServices/ServicesTextService.cs
@@ -1,13 +1,14 @@
 using AutoMapper;
 using WebApi.DTOs.LandingPage.ServiceTexts;
 using WebApi.Entities;
+using WebApi.Repositories.LandingRepositories;
 using WebApi.Repository;
 
 namespace WebApi.Services.LandingServices
 {
     public class ServicesTextService : BaseService<LandingServiceText, LandingServiceAddModel, LandingServiceUpdateModel, LandingServiceViewModel, LandingServiceViewIdModel>
     {
-        public ServicesTextService(IRepository<LandingServiceText> repository, IMapper mapper) : base(repository, mapper)
+        public ServicesTextService(ServiceTextRepository repository, IMapper mapper) : base(repository, mapper)
         {
         }
     }
