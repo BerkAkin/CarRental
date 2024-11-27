a87d41e9036fa74ecbc6647098747b4c296dfdcc
Berk Akın <berkakin99@hotmail.com>
2024-11-27 19:04:43 +0300

    FIX: Repository is now included when inheriting from BaseService

--- backend/WebApi/Services/FAQService/FAQService.cs
+++ backend/WebApi/Services/FAQService/FAQService.cs
@@ -6,7 +6,7 @@ using WebApi.Repository;
 
 namespace WebApi.Services.FAQService
 {
-    public class FAQService : BaseService<FAQText, FAQAddModel, FAQUpdateModel, FAQViewModel, FAQViewIdModel>
+    public class FAQService : BaseService<FAQText, FAQAddModel, FAQUpdateModel, FAQViewModel, FAQViewIdModel, FAQRepository>
     {
         public FAQService(FAQRepository repository, IMapper mapper) : base(repository, mapper) { }
     }
