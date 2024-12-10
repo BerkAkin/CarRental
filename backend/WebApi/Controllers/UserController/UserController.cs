cb0a5027d4d663dbd0c17b2c7e37a0502b262f42
Berk Akın <berkakin99@hotmail.com>
2024-12-10 16:13:50 +0300

    FEAT: Admin's user endpoints and User's own info endpoints implemented

--- backend/WebApi/Controllers/UserController/UserController.cs
+++ backend/WebApi/Controllers/UserController/UserController.cs
@@ -1,14 +1,12 @@
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.DTOs.User;
-using WebApi.Entities;
 using WebApi.Services.UserService;
 
-namespace WebApi.Controllers.UserController
+namespace WebApi.Controllers.UserController.UserController
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]s")]
     public class UserController : BaseController
     {
         private readonly UserService _userService;
@@ -18,13 +16,13 @@ namespace WebApi.Controllers.UserController
         }
 
         [Authorize(Roles = "2")]
-        [HttpGet]
-        public async Task<ActionResult<UserViewIdModel>> GetById()
+        [HttpGet("OwnInfo")]
+        public async Task<ActionResult<UserViewIdModel>> GetOwnInfo()
         {
 
             try
             {
-                var data = await _userService.GetByIdAsync(Convert.ToInt32(UserId));
+                var data = await _userService.GetOwnInfo(Convert.ToInt32(UserId));
                 return Ok(data);
             }
             catch (Exception ex)
@@ -34,13 +32,14 @@ namespace WebApi.Controllers.UserController
             }
 
         }
+
         [Authorize(Roles = "2")]
-        [HttpPut]
-        public async Task<ActionResult> Update([FromBody] UserUpdateModel model)
+        [HttpPut("OwnInfo")]
+        public async Task<ActionResult> UpdateOwnInfo([FromBody] UserUpdateModel model)
         {
             try
             {
-                await _userService.UpdateAsync(Convert.ToInt32(UserId), model);
+                await _userService.UpdateOwnInfo(Convert.ToInt32(UserId), model);
                 return Ok("Güncelleme Başarılı");
             }
             catch (Exception ex)
@@ -49,6 +48,54 @@ namespace WebApi.Controllers.UserController
             }
 
         }
+
+        [Authorize(Roles = "1")]
+        [HttpGet]
+        public async Task<ActionResult<List<AdminUserViewModel>>> GetAllUsers()
+        {
+            var data = await _userService.GetAllAsync();
+            return Ok(data);
+        }
+
+        [Authorize(Roles = "1")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AdminUserViewIdModel>> GetUserById(int id)
+        {
+            var data = await _userService.GetByIdAsync(id);
+            return Ok(data);
+        }
+
+        [Authorize(Roles = "1")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateUserAsync(int id, [FromBody] AdminUserUpdateModel model)
+        {
+            try
+            {
+                await _userService.UpdateAsync(id, model);
+                return Ok("Güncelleme İşlemi başarılı.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
+            }
+        }
+
+        [Authorize(Roles = "1")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUserAsync(int id)
+        {
+            await _userService.DeleteAsync(id);
+            return Ok("Silme İşlemi Başarılı");
+        }
+
+        [Authorize(Roles = "1")]
+        [HttpPut("{id}/is-active")]
+        public async Task<ActionResult> ActivateUser(int id)
+        {
+            await _userService.ActivateUserAsync(id);
+            return Ok("Kullanıcı Aktifleştirildi");
+        }
+
 
     }
 }
