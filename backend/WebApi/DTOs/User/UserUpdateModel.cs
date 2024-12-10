f3c458cb6113668e156c977affb082507e707fd1
Berk Akın <berkakin99@hotmail.com>
2024-12-10 15:49:21 +0300

    FEAT: Added user's role info

--- backend/WebApi/DTOs/User/UserUpdateModel.cs
+++ backend/WebApi/DTOs/User/UserUpdateModel.cs
@@ -6,6 +6,6 @@ namespace WebApi.DTOs.User
         public string Surname { get; set; }
         public string Email { get; set; }
         public string PhoneNum { get; set; }
-        public int RoleId { get; set; }
+
     }
 }
