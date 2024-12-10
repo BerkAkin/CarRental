beb7604e6b1191536b0582dc571e5976950ddad3
Berk Akın <berkakin99@hotmail.com>
2024-12-10 15:47:49 +0300

    FEAT: Added accesstoken check on api calls

--- frontend/src/api/apiService.tsx
+++ frontend/src/api/apiService.tsx
@@ -2,13 +2,26 @@ import axios from "axios";
 import { BASE_URL, endpoints } from "./apiConfig";
 
 async function apiService(endpoint: string, method: string = "GET", data: any = null) {
+
     const url = `${BASE_URL}${endpoint}`;
+    const token = localStorage.getItem("accessToken") || "";
+
+    const headers: Record<string, string> = {
+        "Content-Type": "application/json",
+    };
+
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+    }
 
     try {
-        const response = await axios({ method, url, data, withCredentials: true });
+        const response = await axios({ method, url, data, headers, withCredentials: true });
         return response.data;
     } catch (e) {
-        throw new Error(`${e}`);
+        if (axios.isAxiosError(e)) {
+            console.error('Axios error: ', e.response?.data || e.message);
+        }
+        throw new Error("API request failed");
     }
 }
 
