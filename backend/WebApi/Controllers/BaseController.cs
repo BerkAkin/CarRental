7b24cee4dcedf55281b9d41da7843d5ab06eb999
Berk Akın <berkakin99@hotmail.com>
2024-12-10 15:51:08 +0300

    FEAT: Created basecontroller to get logged in user's id via claim from cookie

---
+++ backend/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected string UserId => User.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+
+}
