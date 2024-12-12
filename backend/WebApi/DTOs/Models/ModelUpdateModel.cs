f3ff996482f6747a539f525447ab6c9ed408827b
Berk Akın <berkakin99@hotmail.com>
2024-12-12 09:58:09 +0300

    FEAT: Model Updater DTO defined

---
+++ backend/WebApi/DTOs/Models/ModelUpdateModel.cs
@@ -0,0 +1,21 @@
+namespace WebApi.DTOs.Models
+{
+    public class ModelUpdateModel
+    {
+
+        public int Id { get; set; }
+        public int FuelTypeId { get; set; }
+        public int GearTypeId { get; set; }
+        public int CarTypeId { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public string Description { get; set; }
+        public int PersonCount { get; set; }
+        public int LuggageCount { get; set; }
+        public int DoorCount { get; set; }
+        public int Price { get; set; }
+        public string[] OtherServices { get; set; }
+        public string[] OtherFeatures { get; set; }
+        public string ImageDirectory { get; set; }
+    }
+}
