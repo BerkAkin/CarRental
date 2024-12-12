7a01e26ea9b6467b126e247a521b250090644f1e
Berk Akın <berkakin99@hotmail.com>
2024-12-12 09:58:17 +0300

    FEAT: Model viewer by id DTO defined

---
+++ backend/WebApi/DTOs/Models/ModelViewIdModel.cs
@@ -0,0 +1,22 @@
+using WebApi.Entities;
+
+namespace WebApi.DTOs.Models
+{
+    public class ModelViewIdModel
+    {
+        public int Id { get; set; }
+        public CarType CarType { get; set; }
+        public FuelType FuelType { get; set; }
+        public GearType GearType { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public string Description { get; set; }
+        public int PersonCount { get; set; }
+        public int LuggageCount { get; set; }
+        public int DoorCount { get; set; }
+        public int Price { get; set; }
+        public string[] OtherServices { get; set; }
+        public string[] OtherFeatures { get; set; }
+        public string ImageDirectory { get; set; }
+    }
+}
