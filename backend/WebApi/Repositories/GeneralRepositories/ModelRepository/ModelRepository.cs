48bb474632da14b975c467c7e5e5c5c5db48dd0e
Berk Akın <berkakin99@hotmail.com>
2024-12-12 16:10:57 +0300

    FEAT: Model repository implemented and added summary repo function with pagination logic.

--- backend/WebApi/Repositories/GeneralRepositories/ModelRepository/ModelRepository.cs
+++ backend/WebApi/Repositories/GeneralRepositories/ModelRepository/ModelRepository.cs
@@ -11,14 +11,37 @@ namespace WebApi.Repositories.GeneralRepositories.ModelRepository
 
         }
 
-        public override async Task<IEnumerable<Model>> GetAllAsync(Func<IQueryable<Model>, IQueryable<Model>> func = null)
+        public async Task<(IEnumerable<Model>, int TotalCounts)> GetAllPaginatedAsync(int pageNumber, int pageSize, Func<IQueryable<Model>, IQueryable<Model>> func = null)
         {
+            IQueryable<Model> data = _context.Set<Model>();
+            if (func is not null)
+            {
+                data = func(data);
+            }
+            int totalCounts = await data.CountAsync();
+            var paginatedData = await data.OrderBy(m => m.Id).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return (paginatedData, totalCounts);
+
+        }
+
+
+        public async Task<(IEnumerable<Model>, int TotalRecords)> GetAllSummaryPaginatedAsync(int pageNumber, int pageSize, Func<IQueryable<Model>, IQueryable<Model>> func = null)
+        {
+
             IQueryable<Model> query = _context.Set<Model>();
+
             if (func != null)
             {
                 query = func(query);
+
             }
-            return await query.ToListAsync();
+
+            var totalRecords = await query.CountAsync();
+            var paginatedData = await query.OrderBy(m => m.Id).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return (paginatedData, totalRecords);
         }
+
     }
 }
