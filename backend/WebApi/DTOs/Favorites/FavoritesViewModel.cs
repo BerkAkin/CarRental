8df0539aa42129f5fda1a9438f182de47d0b3869
Berk Akın <berkakin99@hotmail.com>
2024-12-18 12:26:43 +0300

    FEAT: Favorite view dto defined

---
+++ backend/WebApi/DTOs/Favorites/FavoritesViewModel.cs
@@ -0,0 +1,15 @@
+using WebApi.Entities;
+
+namespace WebApi.DTOs.Favorites
+{
+    public class FavoriteViewModel
+    {
+        public string CarType { get; set; }
+        public string GearType { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public int PersonCount { get; set; }
+        public int Price { get; set; }
+        public string ImageDirectory { get; set; }
+    }
+}
