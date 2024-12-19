6a7b4a6e3be0d9ae213b21e68ea9aa2fa487a478
Berk Akın <berkakin99@hotmail.com>
2024-12-19 13:49:40 +0300

    FEAT: Favorite Repository for favorite operations implemented

--- backend/WebApi/Repositories/GeneralRepositories/FavoriteRepository/FavoriteRepository.cs
+++ backend/WebApi/Repositories/GeneralRepositories/FavoriteRepository/FavoriteRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebApi.DbOperations;
+using WebApi.DTOs.Favorites;
 using WebApi.Entities;
 
 namespace WebApi.Repositories.GeneralRepositories.FavoriteRepository
@@ -8,7 +9,7 @@ namespace WebApi.Repositories.GeneralRepositories.FavoriteRepository
     {
         public FavoriteRepository(CRDbContext context) : base(context) { }
 
-        public async Task<IEnumerable<UserFavorite>> GetAllUserFavoritesAsync(Func<IQueryable<UserFavorite>, IQueryable<UserFavorite>> include = null)
+        public async Task<IEnumerable<UserFavorite>> GetOwnFavoritesAsync(Func<IQueryable<UserFavorite>, IQueryable<UserFavorite>> include = null)
         {
             IQueryable<UserFavorite> query = _context.Set<UserFavorite>();
             if (include != null)
@@ -16,6 +17,17 @@ namespace WebApi.Repositories.GeneralRepositories.FavoriteRepository
                 query = include(query);
             }
             return await query.ToListAsync();
+        }
+
+        public async Task<UserFavorite> CheckFavorite(int UserId, int modelId)
+        {
+            return await _context.UserFavorites.SingleOrDefaultAsync(uf => uf.UserId == UserId && uf.ModelId == modelId);
+        }
+
+        public async Task AddFavorite(UserFavorite entity)
+        {
+            await _context.UserFavorites.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
