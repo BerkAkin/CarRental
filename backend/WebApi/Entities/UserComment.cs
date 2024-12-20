8a7be90370f37af01c66fd84a81203c349a5a7ea
Berk Akın <berkakin99@hotmail.com>
2024-12-20 15:57:22 +0300

    FEAT: Added isActive property to entity

--- backend/WebApi/Entities/UserComment.cs
+++ backend/WebApi/Entities/UserComment.cs
@@ -7,6 +7,7 @@ namespace WebApi.Entities
         public int Id { get; set; }
         public string Content { get; set; }
         public int StarCount { get; set; }
+        public bool IsActive { get; set; } = false;
         public User User { get; set; }
         public int UserId { get; set; }
         public DateTime CreatedAt { get; set; }
