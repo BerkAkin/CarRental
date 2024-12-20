3273e559f69342ce0f3541e0dbc97cbcb97eabb0
Berk Akın <berkakin99@hotmail.com>
2024-12-20 16:02:45 +0300

    STYLE & FEAT: UserProfile is more neat now and added deleteAccount option

--- frontend/src/Components/Containers/UserProfileContainer/UserProfile.tsx
+++ frontend/src/Components/Containers/UserProfileContainer/UserProfile.tsx
@@ -3,8 +3,6 @@ import styles from './styles.module.css';
 import { Formik, Field, Form } from 'formik';
 import apiService from '../../../api/apiService';
 import { endpoints } from '../../../api/apiConfig';
-import ModelCard from '../ModelsContainer/ModelCard/ModelCard';
-import { faV } from '@fortawesome/free-solid-svg-icons';
 import UserFavoriteCard from './UserFavoriteCard/UserFavoriteCard';
 
 
@@ -92,128 +90,146 @@ function UserProfile() {
         }
     }
 
+    const deleteAccount = async () => {
+        try {
+            await apiService(endpoints.ownAccountDelete, "DELETE");
+            alert("OK")
+            await apiService(endpoints.logout, "GET");
+            localStorage.removeItem("accessToken");
+            alert("Çıkış Yapıldı");
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
     return (
         <>
             {isError ? (
                 <div style={{ color: "red" }}>{isError}</div>
             ) : (
-                <div className={`container mt-4 pt-3  ${styles.mainContainer}`}>
-                    <div className='row'>
-                        <div className='col-12'>
-                            <h2 className={styles.headerColor}>Profilim</h2>
-                        </div>
-                        <div className={`${styles.subContainer} container pt-4`}>
-                            <div className='row h-100'>
+                <div className={`container mt-4 pt-3 `}>
+
+                    <ul className="nav nav-tabs mt-2" role="tablist">
+                        <li className="nav-item " >
+                            <a className={`${styles.navBtn} nav-link active`} id="disabled-tab-0" data-bs-toggle="tab" href="#disabled-tabpanel-0">Bilgilerim</a>
+                        </li>
+                        <li className="nav-item" >
+                            <a className={`${styles.navBtn} mx-3 nav-link`} id="disabled-tab-1" data-bs-toggle="tab" href="#disabled-tabpanel-1">Favorilerim</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`${styles.navBtn} nav-link`} id="disabled-tab-2" data-bs-toggle="tab" href="#disabled-tabpanel-2">Yorumum</a>
+                        </li>
+                    </ul>
+
+                    <div className="tab-content pt-3" id="tab-content">
+                        <div className="tab-pane active" id="disabled-tabpanel-0" role="tabpanel" aria-labelledby="disabled-tab-0">
+                            <div className='col'>
                                 {initialInfoValues ? (
-                                    <Formik initialValues={initialInfoValues} onSubmit={submitInfoForm} enableReinitialize>
+                                    <div>
+                                        <Formik initialValues={initialInfoValues} onSubmit={submitInfoForm} enableReinitialize>
+                                            <div>
+                                                <Form>
+                                                    <div className='row mt-3'>
+                                                        <label className={`${styles.label} mb-2`}>E-Posta</label>
+                                                        <Field className={`${styles.infos} `} id="email" name="email" />
+                                                    </div>
+                                                    <div className='row mt-4'>
+                                                        <label className={`${styles.label} mb-2`}>Telefon Numarası</label>
+                                                        <Field className={`${styles.infos} mx-auto`} id="phoneNum" name="phoneNum" />
+                                                    </div>
+                                                    <div className='row mt-4'>
+                                                        <label className={`${styles.label} mb-2`}>Üyelik Tarihi</label>
+                                                        <Field className={`${styles.infos} mx-auto`} id="createdAt" name="createdAt" />
+                                                    </div>
 
-                                        <div className='col-2 border-end'>
-                                            <div className='row'>
-                                                <h4 className={styles.headerColor}>Bilgilerim</h4>
+
+                                                    <div className='row mt-5 d-flex justify-content-center'>
+                                                        <button className={`${styles.btn}`} type='submit'>Bilgilerimi Değiştir</button>
+                                                    </div>
+                                                </Form>
                                             </div>
-                                            <div className='row'>
-                                                <div></div>
-                                            </div>
 
-                                            <Form>
-                                                <div className='row mt-3 border-bottom'>
-                                                    <label>E-Posta</label>
-                                                    <Field className={`${styles.infos} mx-auto`} id="email" name="email" />
-                                                </div>
-                                                <div className='row mt-3 border-bottom'>
-                                                    <label>Telefon Numarası</label>
-
-                                                    <Field className={`${styles.infos} mx-auto`} id="phoneNum" name="phoneNum" />
-                                                </div>
-                                                <div className='row mt-3 border-bottom'>
-                                                    <label>Katılma Tarihi</label>
-                                                    <Field className={`${styles.infos} mx-auto`} id="createdAt" name="createdAt" />
-                                                </div>
-                                                <div className='row mt-5'>Şifre Değiştir</div>
-                                                <div className='row'>Hesabımı Dondur</div>
-                                                <div>
-                                                    <button type='submit'>Bilgilerimi Değiştir</button>
-                                                </div>
-                                            </Form>
+                                        </Formik>
+                                        <div className='row mt-2 d-flex justify-content-center'>
+                                            <button className={`${styles.btn}`}>Şifremi Değiştir</button>
                                         </div>
-                                    </Formik>
+                                        <div className='row mt-2 d-flex justify-content-center'>
+                                            <button className={`${styles.deleteBtn}`} onClick={deleteAccount}>Hesabımı Sil</button>
+                                        </div>
+                                    </div>
+
                                 ) : (
                                     <p>Bilgiler yükleniyor...</p>
                                 )}
+                            </div>
+                        </div>
+                        <div className="tab-pane" id="disabled-tabpanel-1" role="tabpanel" aria-labelledby="disabled-tab-1">
+                            <div className='container mt-4'>
+                                <div className={`${styles.modelsList} row overflow-y-scroll`}>
+                                    {(
+                                        initialFavorites?.map((favorite: FavoriteModels) => {
+                                            return (
+                                                <div className='col-3 mt-3'>
 
+                                                    <UserFavoriteCard removeFavorites={removeFavorite} modelId={favorite.modelId}
+                                                        brandName={favorite.brandName} carType={favorite.carType} gearType={favorite.gearType}
+                                                        imageDirectory={favorite.imageDirectory} modelName={favorite.modelName} personCount={favorite.personCount}
+                                                        price={favorite.price}
+                                                    />
+                                                </div>
 
-                                <div className='col-10'>
-                                    <div className='container h-75'>
-                                        <h4 className={styles.headerColor}>Favorilerim</h4>
-                                        <div className='row'>{
-                                            (
-                                                initialFavorites?.map((favorite: FavoriteModels) => {
-
-                                                    return (
-                                                        <div className='col-3'>
-
-                                                            <UserFavoriteCard removeFavorites={removeFavorite} modelId={favorite.modelId}
-                                                                brandName={favorite.brandName} carType={favorite.carType} gearType={favorite.gearType}
-                                                                imageDirectory={favorite.imageDirectory} modelName={favorite.modelName} personCount={favorite.personCount}
-                                                                price={favorite.price}
-
-                                                            />
-
-                                                        </div>
-                                                    )
-                                                })
                                             )
-                                        }
-
-                                        </div>
-                                    </div>
-
-
-                                    <div className='container border-top h-25'>
-                                        <h4 className={styles.headerColor}>Yorumum</h4>
-                                        <div className='row'>
-                                            <div className=''>
-                                                {initialCommentValues ? (
-                                                    <Formik initialValues={initialCommentValues} onSubmit={submitCommentForm} enableReinitialize>
-                                                        <Form>
-                                                            <div className='row'>
-                                                                <Field as="textarea" rows={3} style={{ resize: "none", height: "100%" }} id="content" name="content" className={`${styles.infos}`} />
-                                                            </div>
-                                                            <div className='row mt-3'>
-                                                                <div>
-                                                                    {[1, 2, 3, 4, 5].map((star) => (
-                                                                        <span
-                                                                            key={star}
-                                                                            onClick={() => setRating(star)}
-                                                                            onMouseEnter={() => setHover(star)}
-                                                                            onMouseLeave={() => setHover(0)}
-                                                                            style={{
-                                                                                cursor: "pointer", color: star <= (hover || rating) ? "gold" : "gray", fontSize: "2rem",
-                                                                            }}>
-                                                                            ☆
-                                                                        </span>
-                                                                    ))}
-                                                                </div>
-                                                                <button type='submit'>Yorumumu Güncelle</button>
-
-
-                                                            </div>
-                                                        </Form>
-                                                    </Formik>
-                                                ) : (
-                                                    <p>Bilgiler yükleniyor...</p>
-                                                )}
-                                            </div>
-                                        </div>
-                                    </div>
-
+                                        })
+                                    )}
                                 </div>
                             </div>
-
-
                         </div>
+                        <div className="tab-pane" id="disabled-tabpanel-2" role="tabpanel" aria-labelledby="disabled-tab-2">
+                            <div className='container'>
+                                <div className='row'>
+                                    <div>
+                                        {initialCommentValues ? (
+                                            <Formik initialValues={initialCommentValues} onSubmit={submitCommentForm} enableReinitialize>
+                                                <Form>
+                                                    <div className='container'>
+                                                        <div className='row'>
+                                                            <div className='ps-0 d-flex justify-content-start align-items-center'>
+                                                                <p style={{ fontSize: "1.1rem", paddingTop: "5px" }}>Bizi Değerlendir !</p>
+                                                                <p className='mx-2'>{[1, 2, 3, 4, 5].map((star) => (
+                                                                    <span
+                                                                        key={star}
+                                                                        onClick={() => setRating(star)}
+                                                                        onMouseEnter={() => setHover(star)}
+                                                                        onMouseLeave={() => setHover(0)}
+                                                                        style={{ cursor: "pointer", color: star <= (hover || rating) ? "gold" : "gray", fontSize: "2rem", }}>
+                                                                        ☆
+                                                                    </span>
+                                                                ))}</p>
 
 
+                                                            </div>
+
+                                                        </div>
+                                                        <div className='row  mt-1'>
+                                                            <Field as="textarea" rows={10} id="content" name="content" className={`${styles.comment}`} />
+                                                        </div>
+                                                        <div className='row  mt-2'>
+                                                            <button className={`${styles.btn} mt-4 `} type='submit'>Yorumumu Güncelle</button>
+                                                        </div>
+                                                    </div>
+
+                                                </Form>
+                                            </Formik>
+                                        ) : (
+                                            <p>Bilgiler yükleniyor...</p>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div >
             )
