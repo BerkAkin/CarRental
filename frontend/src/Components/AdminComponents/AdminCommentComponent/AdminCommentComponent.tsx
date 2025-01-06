f0e75a37e8020fa4242b160a1727993df190425c
Berk Akın <berkakin99@hotmail.com>
2025-01-06 18:46:24 +0300

    FEAT: Admin can now see comments on the application and perform operations such as reject or approve

---
+++ frontend/src/Components/AdminComponents/AdminCommentComponent/AdminCommentComponent.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import styles from "./styles.module.css"
+import { useCommentsContext } from '../../../Contexts/CommentContext';
+
+function AdminCommentComponent() {
+
+    const { comments, acceptComment, refuseComment, nextPage, previousPage } = useCommentsContext();
+
+    return (
+        <div>
+            <table className="table-striped table-hover table m-0 mt-4">
+                <thead>
+                    <tr>
+                        <th scope="col">Ad Soyad</th>
+                        <th scope="col">E-Posta</th>
+                        <th scope="col">Kullanıcı Türü</th>
+                        <th scope="col">Aktif Mi</th>
+                        <th scope="col">Puan</th>
+                        <th scope="col">Yorum</th>
+                        <th scope="col">İşlemler</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {comments ?
+                        (comments?.data.map((item: any) => (
+                            <>
+                                <tr>
+                                    <td scope="row">{item.userName}</td>
+                                    <td>{item.userMail}</td>
+                                    <td>{item.userType}</td>
+                                    <td>{item.isActive === true ? "Evet" : "Hayır"}</td>
+                                    <td>{item.starCount}</td>
+                                    <td>{item.content}</td>
+                                    <td>
+                                        {item.isActive === true ?
+                                            (
+                                                <>
+                                                    <button onClick={() => refuseComment(item.id)} className={`${styles.actionNoBtn} mx-2`}>Reddet</button>
+                                                </>
+                                            )
+                                            :
+                                            (
+                                                <>
+                                                    <button onClick={() => acceptComment(item.id)} className={`${styles.actionOkBtn}`}>Onayla</button>
+                                                </>
+                                            )
+                                        }
+                                    </td>
+                                </tr >
+                            </>
+                        ))
+
+                        )
+                        :
+                        (<></>)}
+                </tbody>
+            </table>
+            <div className='d-flex justify-content-end mt-4'>
+                <button onClick={previousPage} className={`${styles.btn}`} style={{ width: "150px" }}>Önceki Sayfa</button>
+                <button onClick={nextPage} className={`${styles.btn} ms-3`} style={{ width: "150px" }}>Sonraki Sayfa</button>
+            </div>
+        </div>
+    )
+}
+
+export default AdminCommentComponent
