639a34de81685463c43ee0b1d3b3aab466a9fe9e
Berk Akın <berkakin99@hotmail.com>
2025-01-07 16:43:40 +0300

    REFACTOR: Is now a component and directory updated

---
+++ frontend/src/Components/LoginComponent/LoginComponent.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import styles from './styles.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import loginValidationSchema from './LoginValidationSchema';
+import ListElement from '../ListElement/ListElement';
+import logo from '../../assets/logos/logo-flexper.png';
+import Image from '../Image/Image';
+import apiService from '../../api/apiService';
+import { endpoints } from '../../api/apiConfig';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../Contexts/AuthContext';
+import { useToastManagerContext } from '../../Contexts/ToastManagerContext';
+
+
+interface LoginProps {
+    email: string;
+    password: string;
+}
+
+const initialValues = {
+    email: '',
+    password: ''
+}
+
+
+function LoginComponent() {
+
+    const { fetchUserType } = useAuthContext();
+    const { showToast } = useToastManagerContext();
+    const navigate = useNavigate();
+
+
+
+    const onSubmit = async (values: LoginProps, { setSubmitting }: any) => {
+        try {
+            const response = await apiService(endpoints.login, "POST", values)
+            setSubmitting(true);
+            setTimeout(() => {
+                setSubmitting(false);
+            }, 2000);
+            localStorage.setItem("accessToken", response);
+            await fetchUserType();
+            showToast("Giriş Başarılı", "s");
+
+        } catch (error) {
+            console.error(error);
+            showToast(`Başarısız: + ${error}`, "s");
+
+        }
+        finally {
+            setSubmitting(false);
+        }
+    };
+
+
+    return (
+        <>
+            <Formik initialValues={initialValues} validationSchema={loginValidationSchema} onSubmit={onSubmit}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <div className={`${styles.outerContainer} `}>
+                            <div className={`${styles.Form}`}>
+                                <div className='container w-100 h-100'>
+                                    <div className={`${styles.headerArea} row`}>
+                                        <div className='col-6 d-flex justify-content-start align-items-center'>
+                                            <p className={`${styles.headerText} ps-1 pt-4`}>Giriş Yap</p>
+                                        </div>
+                                        <div className='col-6 d-flex justify-content-center pt-2 align-items-center'>
+                                            <div>
+                                                <Image URL={logo} Width='150px'></Image>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className='row '>
+                                        <div className='col-12'>E-Posta<span className={styles.error}> *</span>
+                                            <span className=''> <ErrorMessage name="email" component="span" className={`${styles.error}`} /></span>
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col-12'>
+                                            <Field className={`${styles.inputs}`} type="email" id="email" name="email" />
+                                        </div>
+                                    </div>
+                                    <div className='row mt-3'>
+                                        <div className='col-12'>Parola<span className={styles.error}> *</span>
+                                            <span className=''> <ErrorMessage name="password" component="span" className={`${styles.error}`} /></span>
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col-12'>
+                                            <Field className={`${styles.inputs}`} type="text" id="password" name="password" />
+                                        </div>
+                                    </div>
+                                    <div className='row mt-4'>
+                                        <div className='col-12 '>
+                                            <button className={`${styles.btn}`} type="submit" disabled={isSubmitting}>GİRİŞ YAP</button>
+                                        </div>
+                                    </div>
+                                    <div className='row mt-3'>
+                                        <div className='col-6 ps-3 d-flex justify-content-start'>
+                                            <ListElement href='ParolaSifirla' text={"Şifremi Unuttum"} color='#7a7a7a' fs='0.95rem' isHover={true} hoverColor='#294474' />
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </Form>
+
+                )}
+
+            </Formik>
+
+        </>
+    )
+}
+
+export default LoginComponent
