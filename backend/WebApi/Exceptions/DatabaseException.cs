65d6fdffe5360cb41ac190937be89046ad752659
Berk Akın <berkakin99@hotmail.com>
2025-01-09 23:50:21 +0300

    FEAT: Created custom database esception to hide exception detail and show only the database error on frontend

---
+++ backend/WebApi/Exceptions/DatabaseException.cs
@@ -0,0 +1,10 @@
+namespace WebApi.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException() { }
+        public DatabaseException(string message) : base(message) { }
+        public DatabaseException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+}
