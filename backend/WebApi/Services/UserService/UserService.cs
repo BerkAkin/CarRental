bf9fe4e4c0e056e4394a8f39f26763530ed61001
Berk Akın <berkakin99@hotmail.com>
2025-01-10 19:58:55 +0300

    FEAT: Codes wrapped with try catch blocks to prevent errors and show logical error messages on front-end

--- backend/WebApi/Services/UserService/UserService.cs
+++ backend/WebApi/Services/UserService/UserService.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using WebApi.Common;
 using WebApi.DTOs.AdminDTOs;
 using WebApi.DTOs.User;
 using WebApi.Entities;
+using WebApi.Exceptions;
 using WebApi.Repositories.UserRepository;
 
 namespace WebApi.Services.UserService
@@ -19,84 +21,134 @@ namespace WebApi.Services.UserService
         //Admin Kullanıcıları Getirme
         public override async Task<List<AdminUserViewModel>> GetAllAsync()
         {
+
             var data = await _repository.GetAllAsync(query => query.Include(u => u.Role));
             if (data is null)
             {
-                throw new InvalidOperationException("Veriler Bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USERS_NOT_FOUND);
             }
             return _mapper.Map<List<AdminUserViewModel>>(data);
+
+
+
         }
 
         //Admin Kullanıcıyı Id ile Getirme
         public override async Task<AdminUserViewIdModel> GetByIdAsync(int id)
         {
+
             var data = await _repository.GetByIdAsync(id, query => query.Include(u => u.Role));
             if (data is null)
             {
-                throw new InvalidOperationException("Veri Bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USER_NOT_FOUND);
             }
             return _mapper.Map<AdminUserViewIdModel>(data);
+
         }
 
         //Admin Kullanıcıyı Güncelleme
         public override async Task UpdateAsync(int id, AdminUserUpdateModel model)
         {
+
             var user = await _repository.GetByIdAsync(id);
             if (user == null)
             {
-                throw new KeyNotFoundException("Güncellenecek kullanıcı bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USER_UPDATE_FAIL);
             }
-            user.LastOnline = DateTime.Now;
-            _mapper.Map(model, user);
-            await _repository.UpdateAsync(user);
+
+            try
+            {
+                user.LastOnline = DateTime.Now;
+                _mapper.Map(model, user);
+                await _repository.UpdateAsync(user);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
 
         }
 
         //Admin Kullanıcıyı Silme
         public override async Task DeleteAsync(int id)
         {
+
             var user = await _repository.GetByIdAsync(id);
             if (user is null)
             {
-                throw new KeyNotFoundException("Silinecek kullanıcı bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USER_DELETE_FAIL);
             }
-            user.IsActive = false;
-            await _repository.SaveAsync();
+            try
+            {
+                user.IsActive = false;
+                await _repository.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+
         }
 
         //Admin Kullanıcıyı Aktifleştirme
         public async Task ActivateUserAsync(int id)
         {
+
             var user = await _repository.GetByIdAsync(id);
             if (user is null)
             {
-                throw new KeyNotFoundException("Aktive edilecek kullanıcı bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.ACTIVATE_USER_FAIL);
             }
-            user.IsActive = true;
-            await _repository.SaveAsync();
+
+            try
+            {
+                user.IsActive = true;
+                await _repository.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+
         }
+
+
 
         //Kullanıcı Kendi Bilgilerini Görme
         public async Task<UserViewIdModel> GetOwnInfo(int id)
         {
+
             var user = await _repository.GetByIdAsync(id);
             if (user is null)
             {
-                throw new KeyNotFoundException("Kullanıcı Bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USER_NOT_FOUND);
             }
             return _mapper.Map<UserViewIdModel>(user);
+
+
+
         }
 
         //Kullanıcı Kendi Bilgilerini Güncelleme
         public async Task UpdateOwnInfo(int id, UserUpdateModel model)
         {
+
             var user = await _repository.GetByIdAsync(id);
             if (user is null)
             {
-                throw new KeyNotFoundException("Kullanıcı Bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.USER_UPDATE_FAIL);
             }
-            _mapper.Map(model, user);
-            await _repository.UpdateAsync(user);
+            try
+            {
+                _mapper.Map(model, user);
+                await _repository.UpdateAsync(user);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+
+
         }
 
         //Kullanıcı Kendi Hesabını Silme
