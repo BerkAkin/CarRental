adfa8b24c5697aca11860b4fd8a2bcfa0a00821f
Berk Akın <berkakin99@hotmail.com>
2025-01-10 19:58:33 +0300

    FEAT: Codes wrapped with try catch blocks to prevent errors and show logical error messages on front-end

--- backend/WebApi/Services/GeneralServices/FavoriteService/FavoriteService.cs
+++ backend/WebApi/Services/GeneralServices/FavoriteService/FavoriteService.cs
@@ -1,8 +1,10 @@
 using System.Data;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using WebApi.Common;
 using WebApi.DTOs.Favorites;
 using WebApi.Entities;
+using WebApi.Exceptions;
 using WebApi.Repositories.GeneralRepositories.FavoriteRepository;
 
 namespace WebApi.Services.GeneralServices.FavoriteService
@@ -13,35 +15,59 @@ namespace WebApi.Services.GeneralServices.FavoriteService
 
         public async Task<List<FavoriteViewModel>> GetOwnFavoritesAsync(int UserId)
         {
+
             var favoritesList = await _repository.GetOwnFavoritesAsync(query => query.Include(src => src.Model).ThenInclude(src => src.CarType).Include(src => src.Model.GearType).Where(src => src.UserId == UserId));
             if (favoritesList is null)
             {
-                throw new KeyNotFoundException("Favoriler Bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.FAVORITES_NOT_FOUND);
             }
             return _mapper.Map<List<FavoriteViewModel>>(favoritesList);
+
+
         }
 
         public async Task AddFavorites(int UserId, int modelId)
         {
+
             var exists = await _repository.CheckFavorite(UserId, modelId);
             if (exists is not null)
             {
-                throw new DuplicateNameException("Araç zaten favorilere eklenmiş");
+                throw new DuplicateNameException(ErrorMessages.FAVORITE_ALREADY_EXISTS);
             }
 
-            var userFavorite = new UserFavorite { UserId = UserId, ModelId = modelId };
-            await _repository.AddFavorite(userFavorite);
+            try
+            {
+                var userFavorite = new UserFavorite { UserId = UserId, ModelId = modelId };
+                await _repository.AddFavorite(userFavorite);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+
 
         }
 
         public async Task RemoveFavorite(int UserId, int modelId)
         {
+
             var data = await _repository.CheckFavorite(UserId, modelId);
             if (data is null)
             {
-                throw new KeyNotFoundException("Favori bulunamadı");
+                throw new KeyNotFoundException(ErrorMessages.FAVORITE_DELETE_FAIL);
             }
-            await _repository.DeleteAsync(data);
+            try
+            {
+                await _repository.DeleteAsync(data);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+
         }
+
+
+
     }
 }
