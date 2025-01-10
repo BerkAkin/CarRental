210e4ddc90b53538f589c0d477fe9d6690007976
Berk Akın <berkakin99@hotmail.com>
2025-01-10 19:57:43 +0300

    FIX: Removed unnecessary try catch blocks

--- backend/WebApi/Repositories/GeneralRepositories/FavoriteRepository/FavoriteRepository.cs
+++ backend/WebApi/Repositories/GeneralRepositories/FavoriteRepository/FavoriteRepository.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApi.DbOperations;
 using WebApi.DTOs.Favorites;
 using WebApi.Entities;
+using WebApi.Exceptions;
 
 namespace WebApi.Repositories.GeneralRepositories.FavoriteRepository
 {
@@ -11,23 +12,31 @@ namespace WebApi.Repositories.GeneralRepositories.FavoriteRepository
 
         public async Task<IEnumerable<UserFavorite>> GetOwnFavoritesAsync(Func<IQueryable<UserFavorite>, IQueryable<UserFavorite>> include = null)
         {
+
             IQueryable<UserFavorite> query = _context.Set<UserFavorite>();
             if (include != null)
             {
                 query = include(query);
             }
             return await query.ToListAsync();
+
+
         }
 
         public async Task<UserFavorite> CheckFavorite(int UserId, int modelId)
         {
+
             return await _context.UserFavorites.SingleOrDefaultAsync(uf => uf.UserId == UserId && uf.ModelId == modelId);
+
+
         }
 
         public async Task AddFavorite(UserFavorite entity)
         {
+
             await _context.UserFavorites.AddAsync(entity);
             await _context.SaveChangesAsync();
+
         }
     }
 }
