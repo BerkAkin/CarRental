74c4c05dafc708e7e9511a8abc922fd91a34af4f
Berk Akın <berkakin99@hotmail.com>
2025-01-10 19:56:27 +0300

    FIX: Removed unnecessary try catch blocks

--- backend/WebApi/Controllers/GeneralControllers/CommentController/CommentController.cs
+++ backend/WebApi/Controllers/GeneralControllers/CommentController/CommentController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.DTOs.AdminDTOs;
 using WebApi.DTOs.Comment;
+using WebApi.Exceptions;
 using WebApi.Services.GeneralServices.CommentService;
 
 namespace WebApi.Controllers.GeneralControllers.CommentController
@@ -23,6 +24,7 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
         [HttpGet]
         public async Task<ActionResult<List<AdminCommentViewModel>>> GetAll([FromQuery] int pageNumber = 1)
         {
+
             if (pageNumber < 1)
             {
                 return BadRequest("Sayfa numarası 1 veya daha büyük olmalıdır.");
@@ -42,8 +44,10 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
             {
                 return Ok(response);
             }
+            return NotFound("");
 
-            return NotFound("Bulunamadı");
+
+
 
         }
 
@@ -51,12 +55,16 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
         [HttpGet("/Comments/Latest")]
         public async Task<ActionResult<List<CommentViewModel>>> GetAllLatest()
         {
+
             var data = await _service.GetLatestAsync();
             if (data is not null)
             {
                 return Ok(data);
             }
-            return NotFound("Bulunamadı");
+            return NotFound("");
+
+
+
 
         }
 
@@ -65,31 +73,32 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
         [HttpPut("/Comments/AcceptComment")]
         public async Task<ActionResult> AcceptComment([FromBody] int id)
         {
+
             await _service.AcceptComment(id);
             return Ok("Yorum Doğrulandı");
+
+
         }
 
         [Authorize(Roles = "1")]
         [HttpPut("/Comments/RefuseComment")]
         public async Task<ActionResult> RefuseComment([FromBody] int id)
         {
+
             await _service.RefuseComment(id);
             return Ok("Yorum Kaldırıldı");
+
+
         }
 
         [Authorize(Roles = "1")]
         [HttpGet("{id}")]
         public async Task<ActionResult<CommentViewIdModel>> GetById(int id)
         {
-            try
-            {
-                var result = await _service.GetByIdAsync(id);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                return NotFound(new { message = ex.Message });
-            }
+
+            var result = await _service.GetByIdAsync(id);
+            return Ok(result);
+
         }
 
 
@@ -97,15 +106,10 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
         [HttpPost]
         public async Task<ActionResult> AddAsync([FromBody] CommentAddModel model)
         {
-            try
-            {
-                await _service.AddAsync(model);
-                return Ok("Ekleme İşlemi başarılı.");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
-            }
+
+            await _service.AddAsync(model);
+            return Ok("Ekleme İşlemi başarılı.");
+
         }
 
         [Authorize(Roles = "1,2")]
@@ -120,23 +124,22 @@ namespace WebApi.Controllers.GeneralControllers.CommentController
         [HttpGet("OwnComment")]
         public async Task<ActionResult<CommentViewIdModel>> GetOwnComment()
         {
+
             var comment = await _service.GetOwnComment(Convert.ToInt32(UserId));
             return Ok(comment);
+
+
+
         }
 
         [Authorize(Roles = "1,2")]
         [HttpPut("OwnComment")]
         public async Task<ActionResult> UpdateOwnComment([FromBody] CommentUpdateModel model)
         {
-            try
-            {
-                await _service.UpdateOwnComment(Convert.ToInt32(UserId), model);
-                return Ok("Güncelleme Başarılı");
-            }
-            catch (Exception ex)
-            {
-                return NotFound(ex.Message);
-            }
+
+            await _service.UpdateOwnComment(Convert.ToInt32(UserId), model);
+            return Ok("Güncelleme Başarılı");
+
         }
 
     }
