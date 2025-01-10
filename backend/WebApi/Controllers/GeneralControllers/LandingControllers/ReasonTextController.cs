82480ac697df78143cd815f13670ccd865cf5f6e
Berk Akın <berkakin99@hotmail.com>
2025-01-10 19:56:36 +0300

    FIX: Removed unnecessary try catch blocks

--- backend/WebApi/Controllers/GeneralControllers/LandingControllers/ReasonTextController.cs
+++ backend/WebApi/Controllers/GeneralControllers/LandingControllers/ReasonTextController.cs
@@ -1,9 +1,7 @@
-using FluentValidation;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.DTOs.LandingPage.ReasonTexts;
+using WebApi.Exceptions;
 using WebApi.Services.GeneralServices.LandingServices;
 
 
@@ -23,52 +21,60 @@ namespace WebApi.Controllers.GeneralControllers.LandingControllers
         [HttpGet]
         public async Task<ActionResult<List<LandingReasonViewModel>>> GetAll()
         {
+
             var data = await _service.GetAllAsync();
-            return Ok(data);
+            if (data is not null)
+            {
+                return Ok(data);
+            }
+            return NotFound("Veri bulunamadı");
+
+
         }
         [Authorize(Roles = "1")]
         [HttpGet("{id}")]
         public async Task<ActionResult<LandingReasonViewIdModel>> GetById(int id)
         {
+
             var data = await _service.GetByIdAsync(id);
-            return Ok(data);
+            if (data is not null)
+            {
+                return Ok(data);
+            }
+            return NotFound("Veri bulunamadı");
+
+
         }
 
         [Authorize(Roles = "1")]
         [HttpPost]
         public async Task<ActionResult> AddAsync([FromBody] LandingReasonAddModel model)
         {
-            try
-            {
-                await _service.AddAsync(model);
-                return Ok("Ekleme İşlemi başarılı.");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
-            }
+
+            await _service.AddAsync(model);
+            return Ok("Ekleme İşlemi başarılı.");
+
         }
+
         [Authorize(Roles = "1")]
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateAsync(int id, [FromBody] LandingReasonUpdateModel model)
         {
-            try
-            {
-                await _service.UpdateAsync(id, model);
-                return Ok("Güncelleme İşlemi başarılı.");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Sunucu hatası: {ex.Message}");
-            }
+
+            await _service.UpdateAsync(id, model);
+            return Ok("Güncelleme İşlemi başarılı.");
+
         }
 
         [Authorize(Roles = "1")]
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteAsync(int id)
         {
+
             await _service.DeleteAsync(id);
             return Ok("Silme İşlemi Başarılı");
+
+
         }
     }
 }
