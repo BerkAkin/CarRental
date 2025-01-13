a91f30fa71ad600e8fe1e98f8c396c8bde3be27b
Berk Akın <berkakin99@hotmail.com>
2025-01-13 19:58:46 +0300

    REFACTOR: Removed loading status

--- frontend/src/Contexts/FAQContext.tsx
+++ frontend/src/Contexts/FAQContext.tsx
@@ -8,7 +8,6 @@ const FAQContext = createContext<any>({});
 export const FAQContextProvider = ({ children }: any) => {
     const [FAQs, SetFAQs] = useState<any>();
     const [error, setError] = useState<string>();
-    const [loading, setLoading] = useState<boolean>(true);
 
     const fetchData = useCallback(async () => {
         try {
@@ -19,9 +18,7 @@ export const FAQContextProvider = ({ children }: any) => {
             console.log(error);
             setError("Sık Sorulan Sorular yüklenirken bir hata meydana geldi. Lütfen yöneticinize başvurun");
         }
-        finally {
-            setLoading(false);
-        }
+
 
     }, [])
 
@@ -33,7 +30,7 @@ export const FAQContextProvider = ({ children }: any) => {
     const values = {
         FAQs: FAQs,
         error: error,
-        loading: loading
+
     }
 
     return (
