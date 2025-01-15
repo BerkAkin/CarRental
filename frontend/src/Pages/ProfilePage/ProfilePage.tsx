aaaf59d5187ee5f408a3743233bb344087dc3c03
Berk Akın <berkakin99@hotmail.com>
2025-01-15 17:08:12 +0300

    FEAT: ProfilePage is now checking if the logged in user is really exist via checking user with roleId and Email. This change has been made to prevent unauthorized users to access sensitive admin data by changing the localStorage userRoleId info.

--- frontend/src/Pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/Pages/ProfilePage/ProfilePage.tsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useAuthContext } from '../../Contexts/AuthContext';
 import AdminPage from '../AdminPage/AdminPage';
+import ErrorPage from '../ErrorPage/ErrorPage';
 import UserPage from '../UserPage/UserPage';
 
 function ProfilePage() {
-    const { userTypeId } = useAuthContext();
+
+    const { checkUser } = useAuthContext();
+    const [isUserValid, setIsUserValid] = useState<boolean | null>(null);
+    const [roleId, setRoleId] = useState<number | null>(null);
+
+    useEffect(() => {
+
+        const checkUserStatus = async () => {
+            const result = await checkUser();
+            setIsUserValid(result);
+        };
+
+        const userInfo = JSON.parse(localStorage.getItem("UserInfo") || "{}");
+        setRoleId(userInfo?.roleId);
+        checkUserStatus();
+    }, []);
+
+    if (isUserValid) {
+        if (roleId === 1) {
+            return <AdminPage />;
+        }
+        else if (roleId === 2) {
+            return <UserPage />;
+        }
+    }
+    return <ErrorPage ErrorMessage='Sayfayı görüntülemek için yetkiniz olduğundan emin olun veya giriş yapın' />;
 
 
-    if (userTypeId == "1") {
-        return (
-            <AdminPage />
-        )
-    }
-    else if (userTypeId == "2") {
-        return <> <UserPage /></>
-    }
-    return <></>
 }
 
 export default ProfilePage
