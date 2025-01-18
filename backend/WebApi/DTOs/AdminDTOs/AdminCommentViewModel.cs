de97001973ffbe7136a30752708b8bd9d2711b64
Berk Akın <berkakin99@hotmail.com>
2025-01-18 18:18:56 +0300

    FEAT: Added IsNew property to comment to show new comment badge

--- backend/WebApi/DTOs/AdminDTOs/AdminCommentViewModel.cs
+++ backend/WebApi/DTOs/AdminDTOs/AdminCommentViewModel.cs
@@ -5,7 +5,8 @@ namespace WebApi.DTOs.AdminDTOs
         public int Id { get; set; }
         public string Content { get; set; }
         public int StarCount { get; set; }
-        public bool isActive { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsNew { get; set; }
         public string UserName { get; set; }
         public string UserMail { get; set; }
         public string UserType { get; set; }
