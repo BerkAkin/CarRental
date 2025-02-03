77837a061f05e0aa636ed04d22db38be7636864a
Berk Akın <berkakin99@hotmail.com>
2025-02-03 19:58:50 +0300

    FEAT: register data validator schema defined

--- frontend/src/Components/RegisterComponent/RegisterValidationSchema.tsx
+++ frontend/src/Components/RegisterComponent/RegisterValidationSchema.tsx
@@ -1,11 +1,11 @@
 import * as Yup from 'yup';
 
 const registerValidationSchema = Yup.object({
-    password: Yup.string().required('Zorunludur').min(8, 'En az 8 karakter olmalıdır'),
-    email: Yup.string().email('Geçerli bir e-posta girilmelidir').required('Zorunludur'),
-    name: Yup.string().required("Zorunludur").max(25, 'En fazla 25 karakter olabilir'),
-    surname: Yup.string().required("Zorunludur").max(25, 'En fazla 25 karakter olabilir'),
-    phoneNum: Yup.string().required("Zorunludur").min(11, 'En az 11 karakter olabilir').max(13, 'En fazla 13 karakter olabilir')
+    password: Yup.string().required('Zorunlu').min(8, 'Min 8 karakter olmalıdır'),
+    email: Yup.string().email('Geçerli bir e-posta girilmelidir').matches(/@(gmail\.com|hotmail\.com|outlook\.com)$/, 'Yalnızca gmail veya outlook kullanılabilir.').required('Zorunlu'),
+    name: Yup.string().required("Zorunlu").max(25, 'Maks 25 karakter'),
+    surname: Yup.string().required("Zorunlu").max(25, 'Maks 25 karakter'),
+    phoneNum: Yup.string().matches(/^\d+$/, "Sayı olmalı").required("Zorunlu").min(11, 'Min 11 karakter olabilir').max(13, 'Maks 13 karakter olabilir')
 
 })
 
