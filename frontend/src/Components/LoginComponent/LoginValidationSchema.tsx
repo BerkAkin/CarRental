8a5c6969b16665973f269a0aa965fd5717afe72e
Berk Akın <berkakin99@hotmail.com>
2025-02-03 19:58:46 +0300

    FEAT: login data validator schema defined

--- frontend/src/Components/LoginComponent/LoginValidationSchema.tsx
+++ frontend/src/Components/LoginComponent/LoginValidationSchema.tsx
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
 
 const loginValidationSchema = Yup.object({
-    password: Yup.string().required('Zorunludur'),
-    email: Yup.string().email('Geçerli bir e-posta girilmelidir').required('Zorunludur'),
+    password: Yup.string().min(8, 'Min 8 karakter').required('Zorunlu'),
+    email: Yup.string().email('Geçerli bir e-posta girilmelidir').matches(/@(gmail\.com|hotmail\.com|outlook\.com|mail.com)$/, 'Yalnızca gmail veya outlook kullanılabilir.').required('Zorunludur'),
 })
 
 export default loginValidationSchema; 
