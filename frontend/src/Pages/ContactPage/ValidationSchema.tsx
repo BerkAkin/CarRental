fc0734f56823465a323b8261c06ce2293f2571f8
Berk Akın <berkakin99@hotmail.com>
2025-02-03 19:59:40 +0300

    STYLE: Messages shortened

--- frontend/src/Pages/ContactPage/ValidationSchema.tsx
+++ frontend/src/Pages/ContactPage/ValidationSchema.tsx
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-    name: Yup.string().required('İsim Zorunludur').min(3, 'İsim en az 3 karakter olmalı'),
-    surname: Yup.string().required('Soyisim Zorunludur').min(2, 'Soyisim en az 2 karakter olmalı'),
-    email: Yup.string().email('Geçerli bir e-posta girilmelidir').required('E-Posta Zorunludur'),
-    phone: Yup.number().required('Telefon Numarası Zorunludur').typeError("Telefon Numarası Sayı Olmalıdır"),
-    platform: Yup.string().required('Platform Zorunludur'),
-    permission: Yup.string().required('İzin Zorunludur'),
+    name: Yup.string().required('Zorunlu').min(3, 'Min 3 karakter '),
+    surname: Yup.string().required('Zorunlu').min(2, 'Min 2 karakter'),
+    email: Yup.string().email('Geçerli bir e-posta girilmelidir').required('Zorunlu'),
+    phone: Yup.number().required('Zorunlu').typeError("Sayı olmalı"),
+    platform: Yup.string().required('Zorunlu'),
+    permission: Yup.string().required('Zorunlu'),
     otherPlatform: Yup.string().when('platform', {
         is: 'other',
         then: (schema) => Yup.string().not(["belirtin"], "İfade yanlış").required("Lütfen bir platform adı girin"),
