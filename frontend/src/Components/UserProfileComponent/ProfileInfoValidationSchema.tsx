880bc23671e80d3b76fbbd1082e252b7b42d4409
Berk Akın <berkakin99@hotmail.com>
2025-02-04 20:31:41 +0300

    STYLE: Messsages for outlook and gmail capitalized

--- frontend/src/Components/UserProfileComponent/ProfileInfoValidationSchema.tsx
+++ frontend/src/Components/UserProfileComponent/ProfileInfoValidationSchema.tsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const userProfileInfoValidationSchema = Yup.object({
-    email: Yup.string().email('Geçerli bir e-posta girilmelidir').matches(/@(gmail\.com|hotmail\.com|outlook\.com|mail\.com)$/, 'Yalnızca gmail veya outlook kullanılabilir.').required('Zorunlu'),
+    email: Yup.string().email('Geçerli bir e-posta girilmelidir').matches(/@(gmail\.com|hotmail\.com|outlook\.com|mail\.com)$/, 'Yalnızca Gmail veya Outlook kullanılabilir.').required('Zorunlu'),
     phoneNum: Yup.string().matches(/^\d+$/, "Sayı olmalı").required("Zorunlu").min(11, 'Min 11 karakter ').max(13, 'Maks 13 karakter')
 })
 
