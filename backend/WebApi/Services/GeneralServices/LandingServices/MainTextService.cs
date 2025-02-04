52ed1358ab0c02e9cd13f347c27834a030c121ec
Berk Akın <berkakin99@hotmail.com>
2025-02-04 20:50:37 +0300

    FEAT: Overrided update method and added validations to service

--- backend/WebApi/Services/GeneralServices/LandingServices/MainTextService.cs
+++ backend/WebApi/Services/GeneralServices/LandingServices/MainTextService.cs
@@ -1,7 +1,11 @@
 using AutoMapper;
+using FluentValidation;
+using WebApi.Common;
 using WebApi.DTOs.LandingPage.MainText;
 using WebApi.Entities;
+using WebApi.Exceptions;
 using WebApi.Repositories.GeneralRepositories.LandingRepositories;
+using WebApi.Validators.LandingPage;
 
 
 namespace WebApi.Services.GeneralServices.LandingServices
@@ -13,6 +17,27 @@ namespace WebApi.Services.GeneralServices.LandingServices
         {
         }
 
+        public override async Task UpdateAsync(int id, LandingMainUpdateModel model)
+        {
+            LandingMainValidator validator = new LandingMainValidator();
+            validator.ValidateAndThrow(model);
 
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException(ErrorMessages.GENERAL_UPDATE_FAIL);
+            }
+
+            _mapper.Map(model, entity);
+
+            try
+            {
+                await _repository.UpdateAsync(entity);
+            }
+            catch (Exception)
+            {
+                throw new DatabaseException(ErrorMessages.DATABASE_ERROR);
+            }
+        }
     }
 }
