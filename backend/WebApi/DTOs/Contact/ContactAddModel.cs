38ac5bd93149260e020e5a055e2408a4eb8c8e04
Berk Akın <berkakin99@hotmail.com>
2025-02-06 17:53:43 +0300

    FEAT: Contact add dto defined

---
+++ backend/WebApi/DTOs/Contact/ContactAddModel.cs
@@ -0,0 +1,13 @@
+namespace WebApi.DTOs.Contact
+{
+    public class ContactAddModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Permission { get; set; }
+        public string Platform { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
