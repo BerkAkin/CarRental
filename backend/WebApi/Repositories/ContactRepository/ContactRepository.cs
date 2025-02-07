d81e41cc6bbe2571774bd79deed1fce436b82c5d
Berk Akın <berkakin99@hotmail.com>
2025-02-07 20:19:22 +0300

    FEAT: Contact getter repository function with pagination defined

--- backend/WebApi/Repositories/ContactRepository/ContactRepository.cs
+++ backend/WebApi/Repositories/ContactRepository/ContactRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using WebApi.DbOperations;
 using WebApi.Entities;
 
@@ -6,5 +7,13 @@ namespace WebApi.Repositories.ContactRepository
     public class ContactRepository : BaseRepository<Contact>
     {
         public ContactRepository(CRDbContext context) : base(context) { }
+
+        public async Task<(IEnumerable<Contact>, int)> GetAllPaginatedAsync(int pageNumber, int pageSize)
+        {
+            IQueryable<Contact> data = _context.Set<Contact>();
+            int totalRecords = await data.CountAsync();
+            var paginatedData = await data.OrderByDescending(x => x.Name).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return (paginatedData, totalRecords);
+        }
     }
 }
