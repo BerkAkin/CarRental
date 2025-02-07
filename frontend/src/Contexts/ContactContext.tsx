f27091d1d99f40a818e7e3f8c498161adc64021a
Berk Akın <berkakin99@hotmail.com>
2025-02-07 20:20:16 +0300

    FEAT: Contacts are now fetched with context based logic

--- frontend/src/Contexts/ContactContext.tsx
+++ frontend/src/Contexts/ContactContext.tsx
@@ -1,19 +1,78 @@
-import { createContext, useContext } from "react";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
+import apiService from "../api/apiService";
+import { endpoints } from "../api/apiConfig";
 
 
-interface contactContext {
-
+interface SingleContact {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    phone: string;
+    permission: string;
+    platform: string;
+    isRead: boolean;
 }
 
-const contactContext = createContext<any>("");
+
+interface Contacts {
+    currentPage: number;
+    totalPages: number;
+    totalRecords: number;
+    data: SingleContact[]
+}
+
+const contactContext = createContext<any>({});
 
 export const ContactContextProvider = ({ children }: any) => {
 
+    const [contacts, setContacts] = useState<Contacts>();
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [error, setError] = useState<string>();
 
-
-    const values = {
-
+    const handleNextPage = () => {
+        if (currentPage < (contacts?.totalPages || 1)) {
+            setCurrentPage(prev => prev + 1);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
     }
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(prev => prev - 1);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
+    }
+
+    const fetchContacts = useCallback(async (page: number) => {
+        try {
+            const { data, status }: any = await apiService(endpoints.contactUs + `?pageNumber=${page || 1}`, "GET")
+            setContacts(data);
+        }
+        catch (error) {
+            console.log(error)
+            setError("İletişim istekleri yüklenirken bir hata meydana geldi. Lütfen yöneticinize başvurun");
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchContacts(currentPage);
+    }, [currentPage, fetchContacts])
+
+
+    const values = useMemo(() => ({
+        handleNextPage,
+        handlePreviousPage,
+        contacts,
+        error
+    }), [handleNextPage, handlePreviousPage, contacts, error]);
+
 
     return (
         <contactContext.Provider value={values}>
@@ -23,7 +82,7 @@ export const ContactContextProvider = ({ children }: any) => {
 }
 
 
-export const useConfirmContext = () => {
+export const useContactContext = () => {
     const context = useContext(contactContext);
     if (!context) {
         throw new Error("contactContext must be used within a contactContextProvider");
