82e722c6dcb6b570c80c7ba23f7e7ba62b3dfe57
Berk Akın <berkakin99@hotmail.com>
2025-02-13 17:03:56 +0300

    FIX: Removed unneceassry icon definitions and imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import MainRoutingComponent from "./Components/MainRoutingComponent/MainRoutingComponent";
 import { AuthContextProvider } from "./Contexts/AuthContext";
 import { SliderDataProvider } from "./Contexts/SliderContext";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCar, faParking, faGasPump, faRoad, faKey, faHandSparkles } from '@fortawesome/free-solid-svg-icons';
 import { ToastManagerContextProvider } from "./Contexts/ToastManagerContext";
 import { ConfirmContextProvider } from "./Contexts/ConfirmationContext";
-library.add(faCar, faParking, faGasPump, faRoad, faKey, faHandSparkles)
 
 function App() {
   const signatureOfDev = document.createComment("𓃦")
