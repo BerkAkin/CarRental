74f58581815d8ec2f0194279ba06b35499a5891c
Berk Akın <berkakin99@hotmail.com>
2025-02-13 17:04:40 +0300

    FEAT: ServiceInfo is now have a new interface called IServiceInfoIcon to get name of icon as string from api call

--- frontend/src/Components/ServicesInfo/ServicesInfo.tsx
+++ frontend/src/Components/ServicesInfo/ServicesInfo.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import styles from './styles.module.css'
 import ServiceCard from './ServiceCard/ServiceCard'
-import { faCar, faGasPump, faHandSparkles, faKey, faParking, faRoad } from '@fortawesome/free-solid-svg-icons'
 import img from '../../assets/images/ServiceImages/images'
 import Image from '../../Components/Image/Image'
 
+
+interface IServiceInfoIcon {
+    id: number;
+    name: string;
+}
 interface serviceInfoProps {
-    ServicesLeft: { title: string, content: string, icon: string }[]
-    ServicesRight: { title: string, content: string, icon: string }[]
+    ServicesLeft: { title: string, content: string, icon: IServiceInfoIcon }[]
+    ServicesRight: { title: string, content: string, icon: IServiceInfoIcon }[]
 }
 
 function ServicesInfo({ ServicesLeft, ServicesRight }: serviceInfoProps) {
+
     return (
         <>
             <div className='container mt-5 pt-5 '>
@@ -20,7 +23,7 @@ function ServicesInfo({ ServicesLeft, ServicesRight }: serviceInfoProps) {
                     <div className='col-3 d-flex flex-column justify-content-between'>
                         {ServicesLeft.map((data, index) => (
                             <div key={index} className='row '>
-                                <ServiceCard HeaderTxt={data.title} Icon={data.icon} ParagraphTxt={data.content} />
+                                <ServiceCard HeaderTxt={data.title} Icon={data.icon.name} ParagraphTxt={data.content} />
                             </div>
                         ))}
                     </div>
@@ -32,7 +35,7 @@ function ServicesInfo({ ServicesLeft, ServicesRight }: serviceInfoProps) {
                     <div className='col-3'>
                         {ServicesRight.map((data, index) => (
                             <div key={index} className='row '>
-                                <ServiceCard HeaderTxt={data.title} Icon={data.icon} ParagraphTxt={data.content} />
+                                <ServiceCard HeaderTxt={data.title} Icon={data.icon.name} ParagraphTxt={data.content} />
                             </div>
                         ))}
                     </div>
