4ca92c724f23b96ebd57de0829c31edc6113f5ff
Berk Akın <berkakin99@hotmail.com>
2025-02-26 20:06:49 +0300

    STYLE: Is now responsive

--- frontend/src/Components/WhyInfo/WhyInfo.tsx
+++ frontend/src/Components/WhyInfo/WhyInfo.tsx
@@ -16,17 +16,17 @@ function WhyInfo({ Align, InfoBars, ImgURL, Header }: WhyInfoProp) {
 
                 <div className='row mt-5'>
 
-                    <div className={`col-6  ${Align === true ? '' : 'order-2'}`}>
+                    <div className={`col-lg-6 col-12 ${Align === true ? '' : 'order-2'}`}>
                         <div className='row'>
                             <div className='col-12'>
-                                <h1 style={{ fontSize: "4rem" }} className='text-end'>{Header}</h1>
+                                <h1 style={{ fontSize: "4rem" }} className='text-md-end text-center'>{Header}</h1>
                             </div>
                         </div>
                         {InfoBars.map((info, index) => (
                             <InfoBar key={index} Alignment={Align} HTitle={info.title} PTitle={info.content} />
                         ))}
                     </div>
-                    <div className={`col-6  ${Align === true ? 'order-2' : ''}`}>
+                    <div className={`col-lg-6 col-12 d-none d-lg-block ${Align === true ? 'order-2' : ''}`}>
                         <Image Height='660' Width='660' URL={ImgURL} />
                     </div>
                 </div>
