7a86723f2568b7d4655d50e4472d10965bd0168c
Berk Akın <berkakin99@hotmail.com>
2025-03-03 20:22:20 +0300

    FEAT: Component is now showing skeleton while data is loading

---
+++ frontend/src/Components/Skeletons/SkeletonFAQ/SkeletonFAQ.tsx
@@ -0,0 +1,31 @@
+
+import Skeleton from '../Skeleton/Skeleton'
+import styles from './styles.module.css'
+
+function SkeletonFAQ() {
+
+
+
+
+    return (
+        <div className={`${styles.AccordionContainerSizing} mt-4 pt-3`}>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 mb-4'>
+                        <h2 className={styles.headerColor}>Sık Sorulan Sorular</h2>
+                        <hr></hr>
+                    </div>
+                </div>
+                <div className={`${styles.accordionContainerSkeletonSize} row`}>
+                    <div className='col-12'>
+                        <Skeleton />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default SkeletonFAQ
