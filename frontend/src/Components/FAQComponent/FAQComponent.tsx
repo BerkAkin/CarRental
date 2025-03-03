2006662e997df901d03fc19a8f6038ecc81fb206
Berk Akın <berkakin99@hotmail.com>
2025-03-03 20:20:22 +0300

    FEAT: Component is now shows skeleton while loading

--- frontend/src/Components/FAQComponent/FAQComponent.tsx
+++ frontend/src/Components/FAQComponent/FAQComponent.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Accordion from '../Accordion/Accordion'
 import styles from './styles.module.css'
 import { useFAQContext } from '../../Contexts/FAQContext';
+import SkeletonFAQ from '../Skeletons/SkeletonFAQ/SkeletonFAQ';
 
 function FAQComponent() {
 
-    const { error, FAQs } = useFAQContext();
+    const { error, isLoading, FAQs } = useFAQContext();
 
 
     if (error) return <p>{error}</p>
+    if (isLoading) return <SkeletonFAQ />
+
 
     return (
         <div className={`${styles.AccordionContainerSizing} mt-4 pt-3`}>
+
             <div className='container'>
                 <div className='row'>
                     <div className='col-12 mb-4'>
