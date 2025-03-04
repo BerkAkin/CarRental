194f828f713aaca978b5e0998db6ee31ed3622ed
Berk Akın <berkakin99@hotmail.com>
2025-03-04 17:36:54 +0300

    FEAT: Added skeleton loaders while data is being fetch

--- frontend/src/Components/UserProfileComponent/UserProfileComponent.tsx
+++ frontend/src/Components/UserProfileComponent/UserProfileComponent.tsx
@@ -8,6 +8,7 @@ import { useToastManagerContext } from '../../Contexts/ToastManagerContext';
 import { useConfirmContext } from "../../Contexts/ConfirmationContext";
 import formatDate from "../../common/DateFormatter";
 import userProfileInfoValidationSchema from "./ProfileInfoValidationSchema";
+import Skeleton from "../Skeletons/Skeleton/Skeleton";
 
 
 interface InfoFormProps {
@@ -18,7 +19,7 @@ interface InfoFormProps {
 
 function UserProfileComponent() {
 
-    const { userInfo, loading, error } = useInfoContext();
+    const { userInfo, error } = useInfoContext();
     const { showConfirmation } = useConfirmContext();
     const { showToast } = useToastManagerContext();
 
@@ -36,64 +37,110 @@ function UserProfileComponent() {
     }
 
 
-    if (loading) return <p>Yükleniyor</p>
     if (error) return <p>{error}</p>
     return (
         <>
-            {userInfo ? (
-                <div className={`${styles.bgColor} container border my-2 p-3`}>
-                    <Formik validationSchema={userProfileInfoValidationSchema} initialValues={userInfo} onSubmit={updateUserInfo} enableReinitialize>
+            {userInfo ?
+                (
+                    <div className={`${styles.bgColor} container border my-2 p-3`}>
+                        <Formik validationSchema={userProfileInfoValidationSchema} initialValues={userInfo} onSubmit={updateUserInfo} enableReinitialize>
+                            <div>
+                                <div className="row">
+                                    <h4 className="text-muted text-end">Yeniden Hoş Geldin, {userInfo.name} {userInfo.surname} !</h4>
+                                </div>
+                                <div className="row ">
+                                    <div className="col-12 ">
+
+                                        <Form>
+                                            <div className='row mt-4 '>
+                                                <div className='col-12'>E-Posta<span className={styles.error}> *</span>
+                                                    <span className=''> <ErrorMessage name="email" component="span" className={`${styles.error}`} /></span>
+                                                </div>
+                                            </div>
+                                            <div className='row'>
+                                                <div className='col-12'>
+                                                    <Field className={`${styles.infos} `} id="email" name="email" />
+                                                </div>
+                                            </div>
+                                            <div className='row mt-4'>
+                                                <div className='col-12'>Telefon Numarası<span className={styles.error}> *</span>
+                                                    <span className=''> <ErrorMessage name="phoneNum" component="span" className={`${styles.error}`} /></span>
+                                                </div>
+                                            </div>
+                                            <div className='row'>
+                                                <div className='col-12'>
+                                                    <Field className={`${styles.infos} mx-auto`} id="phoneNum" name="phoneNum" />
+                                                </div>
+                                            </div>
+
+                                            <div className='row mt-4 text-muted'>
+                                                <label className={`${styles.label} mb-2`}>Üyelik Tarihi: {formatDate(userInfo.createdAt)}</label>
+                                            </div>
+                                            <div className='row mt-4 d-flex justify-content-center'>
+                                                <div className='col-12 my-4'>
+                                                    <button className={`${styles.btn}`} type='submit'>Bilgilerimi Değiştir</button>
+                                                </div>
+                                            </div>
+                                        </Form>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </Formik>
+
+                    </div>
+
+                ) : (
+                    <div className={`${styles.bgColor} container border my-2 p-3`}>
+
                         <div>
                             <div className="row">
-                                <h4 className="text-muted text-end">Yeniden Hoş Geldin, {userInfo.name} {userInfo.surname} !</h4>
+                                <h4 className="text-muted text-end"><Skeleton /></h4>
                             </div>
                             <div className="row ">
                                 <div className="col-12 ">
 
-                                    <Form>
-                                        <div className='row mt-4 '>
-                                            <div className='col-12'>E-Posta<span className={styles.error}> *</span>
-                                                <span className=''> <ErrorMessage name="email" component="span" className={`${styles.error}`} /></span>
-                                            </div>
-                                        </div>
-                                        <div className='row'>
-                                            <div className='col-12'>
-                                                <Field className={`${styles.infos} `} id="email" name="email" />
-                                            </div>
-                                        </div>
-                                        <div className='row mt-4'>
-                                            <div className='col-12'>Telefon Numarası<span className={styles.error}> *</span>
-                                                <span className=''> <ErrorMessage name="phoneNum" component="span" className={`${styles.error}`} /></span>
-                                            </div>
-                                        </div>
-                                        <div className='row'>
-                                            <div className='col-12'>
-                                                <Field className={`${styles.infos} mx-auto`} id="phoneNum" name="phoneNum" />
-                                            </div>
-                                        </div>
 
-                                        <div className='row mt-4 text-muted'>
-                                            <label className={`${styles.label} mb-2`}>Üyelik Tarihi: {formatDate(userInfo.createdAt)}</label>
+                                    <div className='row mt-4 '>
+                                        <div className='col-12'>
+                                            <Skeleton />
+
                                         </div>
-                                        <div className='row mt-4 d-flex justify-content-center'>
-                                            <div className='col-12 my-4'>
-                                                <button className={`${styles.btn}`} type='submit'>Bilgilerimi Değiştir</button>
-                                            </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col-12'>
+                                            <Skeleton />
                                         </div>
-                                    </Form>
+                                    </div>
+                                    <div className='row mt-4'>
+                                        <div className='col-12'>
+                                            <Skeleton />
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col-12'>
+                                            <Skeleton />
+                                        </div>
+                                    </div>
+
+                                    <div className='row mt-4 text-muted'>
+                                        <Skeleton />
+                                    </div>
+
+
                                 </div>
 
                             </div>
 
                         </div>
 
-                    </Formik>
 
-                </div>
 
-            ) : (
-                <p>Bilgiler yükleniyor...</p>
-            )}
+                    </div>
+
+                )}
         </>
     )
 }
