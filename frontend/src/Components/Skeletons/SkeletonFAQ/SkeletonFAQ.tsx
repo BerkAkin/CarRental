0b630b76a17693be3adc13308a620e02bca8e626
Berk Akın <berkakin99@hotmail.com>
2025-03-04 22:32:37 +0300

    STYLE: Removed header info of page

--- frontend/src/Components/Skeletons/SkeletonFAQ/SkeletonFAQ.tsx
+++ frontend/src/Components/Skeletons/SkeletonFAQ/SkeletonFAQ.tsx
@@ -9,13 +9,13 @@ function SkeletonFAQ() {
 
     return (
         <div className={`${styles.AccordionContainerSizing} mt-4 pt-3`}>
-            <div className='container'>
-                <div className='row'>
+            <div className='container mt-4 pt-3'>
+                {/*  <div className='row'>
                     <div className='col-12 mb-4'>
                         <h2 className={styles.headerColor}>Sık Sorulan Sorular</h2>
                         <hr></hr>
                     </div>
-                </div>
+                </div> */}
                 <div className={`${styles.accordionContainerSkeletonSize} row`}>
                     <div className='col-12'>
                         <Skeleton />
