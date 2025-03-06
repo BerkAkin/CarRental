6ee1d4aa9f43331b2c72fa219fc073222f05ca29
Berk Akın <berkakin99@hotmail.com>
2025-03-06 22:11:59 +0300

    REFACTOR: Component overhauled. Is now shows summary info of models and will show details when clicked on edit button

--- frontend/src/Components/AdminComponents/AdminModelsComponent/AdminModelsComponent.tsx
+++ frontend/src/Components/AdminComponents/AdminModelsComponent/AdminModelsComponent.tsx
@@ -1,8 +1,8 @@
 import styles from "./styles.module.css"
 import { useTypesContext } from '../../../Contexts/TypesContext'
 import { useModelsContext } from '../../../Contexts/ModelsContext';
-import AdminModelEditCardComponent from "../AdminModelEditCardComponent/AdminModelEditCardComponent";
 import SkeletonTable from "../../Skeletons/SkeletonTable/SkeletonTable";
+import AdminModelList from "../AdminModelList/AdminModelList";
 
 
 
@@ -32,28 +32,21 @@ function AdminModelsComponent({ addNewFunc }: any) {
                                     models && gears && carTypes && fuels ?
                                         (
                                             <div className="table-responsive">
-                                                <table className={`${styles.tableFont} table-hover table mt-2`}>
+                                                <table className={`${styles.tableFont} border table-hover table mt-2`}>
                                                     <thead>
-                                                        <tr>
-                                                            <th>Fotoğraf</th>
+                                                        <tr className="text-center">
+
                                                             <th>Marka</th>
                                                             <th>Model</th>
+                                                            <th>Tip</th>
                                                             <th>Yakıt</th>
                                                             <th>Şanzıman</th>
-                                                            <th>Tip</th>
-                                                            <th className='text-center'>Kişi</th>
-                                                            <th className='text-center'>Bagaj</th>
-                                                            <th className='text-center'>Kapı</th>
-                                                            <th className='text-center'>Ücret</th>
-                                                            <th>Açıklama</th>
-                                                            <th>Diğer Hizmetler</th>
-                                                            <th>Diğer Özellikler</th>
-                                                            <th>İşlemler</th>
+                                                            <th></th>
                                                         </tr>
                                                     </thead>
 
                                                     {models?.data.map((item: any) => (
-                                                        <AdminModelEditCardComponent Item={item} CarTypes={carTypes} Fuels={fuels} Gears={gears} />
+                                                        <AdminModelList Item={item} CarTypes={carTypes} Fuels={fuels} Gears={gears} />
 
                                                     ))}
                                                 </table>
@@ -67,8 +60,8 @@ function AdminModelsComponent({ addNewFunc }: any) {
                                 }
                             </div>
                             <div className='d-flex justify-content-end my-4'>
-                                <button onClick={HandlePreviousModelPage} className={`${styles.btn} mx-3`} style={{ width: "150px" }}>Önceki Sayfa</button>
-                                <button onClick={HandleNextModelPage} className={`${styles.btn}`} style={{ width: "150px" }}>Sonraki Sayfa</button>
+                                <button onClick={HandlePreviousModelPage} className={`${styles.btn} mx-3`} style={{ width: "40px" }}>←</button>
+                                <button onClick={HandleNextModelPage} className={`${styles.btn}`} style={{ width: "40px" }}>→</button>
                             </div>
 
                         </>
