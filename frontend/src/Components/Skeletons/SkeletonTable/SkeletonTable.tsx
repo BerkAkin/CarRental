32d68b311bbe294ad5a40bcf580b04e016caf61b
Berk Akın <berkakin99@hotmail.com>
2025-03-06 18:57:42 +0300

    FEAT: Created skeleton table for showing on admin tables while loading operation is persisting

---
+++ frontend/src/Components/Skeletons/SkeletonTable/SkeletonTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Skeleton from '../Skeleton/Skeleton'
+
+function SkeletonTable() {
+    return (
+        <div className={`container-fluid my-2`}>
+            <Skeleton />
+        </div>
+    )
+}
+
+export default SkeletonTable
