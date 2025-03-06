2c8486a14f9566dd4af6add2ff408612c95ee29e
Berk Akın <berkakin99@hotmail.com>
2025-03-06 18:56:10 +0300

    FEAT: Added skeleton loader

--- frontend/src/Components/AdminComponents/AdminModelsComponent/AdminModelsComponent.tsx
+++ frontend/src/Components/AdminComponents/AdminModelsComponent/AdminModelsComponent.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.css"
 import { useTypesContext } from '../../../Contexts/TypesContext'
 import { useModelsContext } from '../../../Contexts/ModelsContext';
 import AdminModelEditCardComponent from "../AdminModelEditCardComponent/AdminModelEditCardComponent";
+import SkeletonTable from "../../Skeletons/SkeletonTable/SkeletonTable";
 
 
 
@@ -9,57 +10,72 @@ import AdminModelEditCardComponent from "../AdminModelEditCardComponent/AdminMod
 function AdminModelsComponent({ addNewFunc }: any) {
 
     const { gears, fuels, carTypes } = useTypesContext();
-    const { HandleNextModelPage, HandlePreviousModelPage, models, error } = useModelsContext();
-
-    if (error) return <p>{error}</p>
+    const { HandleNextModelPage, HandlePreviousModelPage, models, error, isLoading } = useModelsContext();
 
     return (
+
         <div className={`${styles.inner} border container my-2`}>
-            <div className='d-flex justify-content-end my-4'>
-                <button onClick={addNewFunc} className={`${styles.addBtn}`} style={{ width: "150px" }}>Yeni Model Ekle</button>
-            </div>
-            <div className='row'>
-                {
-                    models && gears && carTypes && fuels ?
-                        (
-                            <div className="table-responsive">
-                                <table className={`${styles.tableFont} table-hover table mt-2`}>
-                                    <thead>
-                                        <tr>
-                                            <th>Fotoğraf</th>
-                                            <th>Marka</th>
-                                            <th>Model</th>
-                                            <th>Yakıt</th>
-                                            <th>Şanzıman</th>
-                                            <th>Tip</th>
-                                            <th className='text-center'>Kişi</th>
-                                            <th className='text-center'>Bagaj</th>
-                                            <th className='text-center'>Kapı</th>
-                                            <th className='text-center'>Ücret</th>
-                                            <th>Açıklama</th>
-                                            <th>Diğer Hizmetler</th>
-                                            <th>Diğer Özellikler</th>
-                                            <th>İşlemler</th>
-                                        </tr>
-                                    </thead>
+            {
+                isLoading ?
+                    <div className='d-flex justify-content-end my-4'>
+                        <SkeletonTable />
+                    </div>
 
-                                    {models?.data.map((item: any) => (
-                                        <AdminModelEditCardComponent Item={item} CarTypes={carTypes} Fuels={fuels} Gears={gears} />
-
-                                    ))}
-                                </table>
+                    :
+                    (
+                        <>
+                            <div className='d-flex justify-content-end my-4'>
+                                <button onClick={addNewFunc} className={`${styles.addBtn}`} style={{ width: "150px" }}>Yeni Model Ekle</button>
                             </div>
-                        )
-                        :
-                        (
-                            <>Modeller Yüklenemedi</>
-                        )
-                }
-            </div>
-            <div className='d-flex justify-content-end my-4'>
-                <button onClick={HandlePreviousModelPage} className={`${styles.btn} mx-3`} style={{ width: "150px" }}>Önceki Sayfa</button>
-                <button onClick={HandleNextModelPage} className={`${styles.btn}`} style={{ width: "150px" }}>Sonraki Sayfa</button>
-            </div>
+                            <div className='row'>
+                                {
+                                    models && gears && carTypes && fuels ?
+                                        (
+                                            <div className="table-responsive">
+                                                <table className={`${styles.tableFont} table-hover table mt-2`}>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Fotoğraf</th>
+                                                            <th>Marka</th>
+                                                            <th>Model</th>
+                                                            <th>Yakıt</th>
+                                                            <th>Şanzıman</th>
+                                                            <th>Tip</th>
+                                                            <th className='text-center'>Kişi</th>
+                                                            <th className='text-center'>Bagaj</th>
+                                                            <th className='text-center'>Kapı</th>
+                                                            <th className='text-center'>Ücret</th>
+                                                            <th>Açıklama</th>
+                                                            <th>Diğer Hizmetler</th>
+                                                            <th>Diğer Özellikler</th>
+                                                            <th>İşlemler</th>
+                                                        </tr>
+                                                    </thead>
+
+                                                    {models?.data.map((item: any) => (
+                                                        <AdminModelEditCardComponent Item={item} CarTypes={carTypes} Fuels={fuels} Gears={gears} />
+
+                                                    ))}
+                                                </table>
+                                            </div>
+                                        )
+                                        :
+                                        (
+
+                                            <SkeletonTable />
+                                        )
+                                }
+                            </div>
+                            <div className='d-flex justify-content-end my-4'>
+                                <button onClick={HandlePreviousModelPage} className={`${styles.btn} mx-3`} style={{ width: "150px" }}>Önceki Sayfa</button>
+                                <button onClick={HandleNextModelPage} className={`${styles.btn}`} style={{ width: "150px" }}>Sonraki Sayfa</button>
+                            </div>
+
+                        </>
+                    )
+
+            }
+
 
         </div>
     )
